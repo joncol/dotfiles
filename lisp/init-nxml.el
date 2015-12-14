a8eed21e1fde2fc871196276e4f7d8200ed90b6a
Jonas Collberg <jonas.collberg@gmail.com>
2015-12-14 12:09:35 +0100

    Add command to reload RNG schema

--- lisp/init-nxml.el
+++ lisp/init-nxml.el
@@ -4,4 +4,19 @@
 
 (add-to-list 'auto-mode-alist '("\\.rnc\\'" . rnc-mode))
 
+(when (eq system-type 'windows-nt)
+  (setq rnc-jing-jar-file "c:/tools/jing/bin"))
+
+(define-key nxml-mode-map (kbd "C-c C-r") 'jco/rng-reload-schema)
+
+(defun jco/rng-reload-schema ()
+  (interactive)
+  (let ((schema-filename rng-current-schema-file-name))
+    (when schema-filename
+      (setq rng-current-schema (rng-load-schema schema-filename))
+      (run-hooks 'rng-schema-change-hook)
+      (message "Reloaded schema %s" schema-filename))
+    (unless schema-filename
+      (rng-set-schema-and-validate))))
+
 (provide 'init-nxml)
