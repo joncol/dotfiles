4ae0a7f149825a1e5238f28c1b7da149e9c9214b
Jonas Collberg <jonas.collberg@gmail.com>
2016-03-18 13:56:49 +0100

    Do not treat _ as a word separator in nsis-mode

--- lisp/init-nsis.el
+++ lisp/init-nsis.el
@@ -1,6 +1,7 @@
 (add-hook 'nsis-mode-hook
           (lambda ()
             (fci-mode)
+            (modify-syntax-entry ?_ "w") ;; do not treat "_" as a word separator
             (no-final-newline)))
 
 (provide 'init-nsis)
