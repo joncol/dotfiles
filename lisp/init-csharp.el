2b0c00487034148a4cd771e296c0eee5ff5f1b25
Jonas Collberg <jonas.collberg@gmail.com>
2016-03-31 13:12:51 +0200

    Disable omnisharp

--- lisp/init-csharp.el
+++ lisp/init-csharp.el
@@ -3,7 +3,7 @@
             (electric-pair-mode 0)
             (add-to-list 'company-backends 'company-omnisharp)
             (c-set-style "c#")
-            (omnisharp-mode)
+            ;; (omnisharp-mode)
             (flycheck-mode)
             (local-set-key (kbd "M-g") 'omnisharp-go-to-definition)))
 
