40d74bce6c0f7eb0c0adcf81575538e277d74727
Jonas Collberg <jonas.collberg@gmail.com>
2016-04-15 11:10:21 +0200

    Change comment style for qml-mode

--- lisp/init-qml.el
+++ lisp/init-qml.el
@@ -1,3 +1,8 @@
 (add-to-list 'auto-mode-alist '("\\.qml$" . qml-mode))
 
+(add-hook 'qml-mode-hook
+          (lambda ()
+            (setq comment-start "//"
+                  comment-end "")))
+
 (provide 'init-qml)
