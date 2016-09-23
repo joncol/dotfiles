2ae5f64db65ca66e3fcaee36b10287073acf2ab6
Jonas Collberg <jonas.collberg@orzone.com>
2016-09-23 17:04:31 +0200

    Turn off indentation when using ya-snippets in Haskell mode

--- lisp/init-haskell.el
+++ lisp/init-haskell.el
@@ -1,10 +1,5 @@
-(defun newline-and-indent-relative ()
-  (interactive)
-  (newline)
-  (indent-to-column (save-excursion
-                      (forward-line -1)
-                      (back-to-indentation)
-                      (current-column))))
+(defun set-yasnippet-fixed-indent ()
+  (setq-local yas-indent-line 'fixed))
 
 (add-hook 'haskell-mode-hook
           (lambda ()
@@ -22,13 +17,15 @@
             (hindent-mode)
             (haskell-indentation-mode)
 
-            ;; (global-unset-key [tab])
-            ;; (local-set-key [tab] (lambda ()
-            ;;                        (interactive)
-            ;;                        (tab-indent-or-complete 1)))
-            ;; (setq tab-stop-list
-            ;;       (loop for i from 0 upto 120 by 2 collect i))
-            ;; (setq evil-shift-width 2)
+            (global-unset-key [tab])
+            (local-set-key [tab] (lambda ()
+                                   (interactive)
+                                   (tab-indent-or-complete 1)))
+            (setq tab-stop-list
+                  (loop for i from 0 upto 120 by 2 collect i))
+            (setq evil-shift-width 2)
+            (set-yasnippet-fixed-indent)
+
             (company-mode)
             ;; (define-key haskell-indentation-mode-map (kbd "RET") nil)
             ;; (define-key global-map (kbd "RET") 'newline-and-indent-relative)
@@ -36,7 +33,7 @@
             (add-to-list 'company-backends 'company-ghc)
             (setq company-ghc-show-info t)
 
-            ;; (define-key yas-minor-mode-map (kbd "TAB") nil)
+            (define-key yas-minor-mode-map (kbd "TAB") nil)
             ;; (define-key evil-motion-state-map (kbd "RET") nil)
             (define-key evil-normal-state-map (kbd "M-.") nil)))
 
