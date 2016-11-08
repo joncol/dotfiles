896ba373819701d631ebc262302c980a260e736b
Jonas Collberg <jonas.collberg@gmail.com>
2016-11-08 15:02:28 +0100

    Remove empty custom-set-variables

--- init.el
+++ init.el
@@ -27,9 +27,3 @@
  '(send-mail-function (quote smtpmail-send-it)))
 
 (put 'narrow-to-region 'disabled nil)
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
