35c14a91e257068e3ee25b9aa44cad2242fda4bb
Jonas Collberg <jonas.collberg@orzone.com>
2016-11-14 16:45:50 +0100

    Set code snippet CSS colors according to theme on export

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -126,4 +126,19 @@
             (add-link-type "yt")
             (add-link-type "ytnc" "&controls=0")))
 
+(defun my/org-inline-css-hook (exporter)
+  "Insert custom inline css to automatically set the
+foreground and background of code, to the current theme's colors"
+  (when (eq exporter 'html)
+    (let* ((my-pre-bg (face-background 'default))
+           (my-pre-fg (face-foreground 'default)))
+      (setq
+       org-html-head-extra
+       (concat
+        org-html-head-extra
+        (format "<style type=\"text/css\">\n pre.src {background-color: %s; color: %s;}</style>\n"
+                my-pre-bg my-pre-fg))))))
+
+(add-hook 'org-export-before-processing-hook 'my/org-inline-css-hook)
+
 (provide 'init-org)
