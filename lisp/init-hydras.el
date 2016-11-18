331441193237de8886a40501ea4ecc7c5ba3c823
Jonas Collberg <jonas.collberg@gmail.com>
2016-11-19 00:57:06 +0100

    Add apropos commands to hydra menu

--- lisp/init-hydras.el
+++ lisp/init-hydras.el
@@ -2,13 +2,34 @@
 
 (global-set-key (kbd "<f1>") 'jco/hydra-main-menu/body)
 
+(defun open-config-file (file-name)
+  (interactive)
+  (find-file (concat user-emacs-directory file-name)))
+
 (defhydra jco/hydra-main-menu (:color blue)
-  "common ops"
-  ("e" (lambda ()
-         (interactive)
-         (find-file (concat user-emacs-directory "init.el")))
-   "edit config")
-  ("m" (mu4e) "mail")
-  ("h" (monky-status) "monky status"))
+  "Menu"
+  ("e" jco/hydra-edit-config/body "edit config")
+  ("m" mu4e "mail")
+  ("h" monky-status "monky status")
+  ("a" jco/hydra-apropos/body "apropos")
+  )
+
+(defhydra jco/hydra-edit-config (:color blue)
+  "Edit config"
+  ("c" (open-config-file "lisp/init-common.el") "common")
+  ("t" (open-config-file "lisp/init-theme.el") "theme")
+  )
+
+(defhydra jco/hydra-apropos (:color blue :hint nil)
+  "Apropos"
+  ("a" apropos "apropos")
+  ("c" apropos-command "cmd")
+  ("d" apropos-documentation "doc")
+  ("e" apropos-value "val")
+  ("l" apropos-library "lib")
+  ("o" apropos-user-option "option")
+  ("v" apropos-variable "var")
+  ("i" info-apropos "info")
+  ("t" tags-apropos "tags"))
 
 (provide 'init-hydras)
