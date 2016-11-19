9333fc9848b5b3abd917325e1c16fb443b92cce9
Jonas Collberg <jonas.collberg@gmail.com>
2016-11-19 23:50:50 +0100

    Add custom agenda view
    Based on http://blog.aaronbieber.com/2016/09/25/agenda-interactions-primer.html.

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -24,6 +24,11 @@
                                                (find-file
                                                 (concat org-directory
                                                         "/work.org"))))))
+
+(jco/define-bindings org-agenda-mode-map
+                     '(("j" . org-agenda-next-item)
+                       ("k" . org-agenda-previous-item)))
+
 (setq org-reveal-hlevel 2)
 (setq org-todo-keyword-faces
       '(("TODO" . "deep pink")
@@ -46,6 +51,41 @@
          :empty-lines-before 0)))
 
 (setq org-log-into-drawer t)
+
+(setq org-agenda-custom-commands
+      '(("c" "Simple agenda view"
+         ((tags "PRIORITY=\"A\""
+                ((org-agenda-skip-function '(org-agenda-skip-entry-if
+                                             'todo 'done))
+                 (org-agenda-overriding-header
+                  "High-priority unfinished tasks:")))
+          (agenda "" ((org-agenda-ndays 1)))
+          (alltodo ""
+                   ((org-agenda-skip-function
+                     '(or (jco/org-skip-subtree-if-habit)
+                          (jco/org-skip-subtree-if-priority ?A)
+                          (org-agenda-skip-if nil '(scheduled deadline))))
+                    (org-agenda-overriding-header
+                     "All normal priority tasks:"))))
+         ((org-agenda-compact-blocks nil)))))
+
+(defun jco/org-skip-subtree-if-priority (priority)
+  "Skip an agenda subtree if it has a priority of PRIORITY.
+
+PRIORITY may be one of the characters ?A, ?B, or ?C."
+  (let ((subtree-end (save-excursion (org-end-of-subtree t)))
+        (pri-value (* 1000 (- org-lowest-priority priority)))
+        (pri-current (org-get-priority (thing-at-point 'line t))))
+    (if (= pri-value pri-current)
+        subtree-end
+      nil)))
+
+(defun jco/org-skip-subtree-if-habit ()
+  "Skip an agenda entry if it has a STYLE property equal to \"habit\"."
+  (let ((subtree-end (save-excursion (org-end-of-subtree t))))
+    (if (string= (org-entry-get nil "STYLE") "habit")
+        subtree-end
+      nil)))
 
 (org-babel-do-load-languages
  'org-babel-load-languages
