79ede1236ffe5482d3649c653cc38019947f1abe
Jonas Collberg <jonas.collberg@gmail.com>
2016-11-19 21:22:52 +0100

    Fix issue with first item in company completion menu being shown to the right

--- lisp/init-company-mode.el
+++ lisp/init-company-mode.el
@@ -1,5 +1,14 @@
 (add-hook 'after-init-hook 'global-company-mode)
 
+(defun on-off-fci-before-company(command)
+  "Fixes the issue where the first item is shown far off to the right."
+  (when (string= "show" command)
+    (turn-off-fci-mode))
+  (when (string= "hide" command)
+    (turn-on-fci-mode)))
+
+(advice-add 'company-call-frontends :before #'on-off-fci-before-company)
+
 (add-hook 'company-mode-hook
           (lambda ()
             ;; (add-to-list 'company-backends 'company-dabbrev-code)
@@ -11,6 +20,6 @@
 
             (jco/define-bindings company-active-map
                                  '(("j" . company-select-next-or-abort)
-                                   ("k" . company-select-previo-or-abort)))))
+                                   ("k" . company-select-previous-or-abort)))))
 
 (provide 'init-company-mode)
