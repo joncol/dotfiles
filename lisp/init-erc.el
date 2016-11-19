e6219b815d01129fd7aecdceb9c6402d51946fce
Jonas Collberg <jonas.collberg@gmail.com>
2016-11-19 20:54:04 +0100

    Update erc settings

--- lisp/init-erc.el
+++ lisp/init-erc.el
@@ -3,4 +3,11 @@
     (setq erc-nick (car acc))
     (setq erc-password (nth 1 acc))))
 
+(add-hook 'erc-mode-hook
+          (lambda ()
+            (setq erc-lurker-hide-list '("JOIN" "PART" "QUIT"))
+            (setq erc-lurker-threshold-time 3600)
+            (setq erc-autojoin-channels-alist
+                  '(("freenode.net" "#emacs" "#haskell" "#programming")))))
+
 (provide 'init-erc)
