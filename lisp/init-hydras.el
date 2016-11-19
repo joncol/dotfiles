df736bd4e306930373c639f0680d382691fe3dd2
Jonas Collberg <jonas.collberg@gmail.com>
2016-11-20 00:19:00 +0100

    Fix hydra colors

--- lisp/init-hydras.el
+++ lisp/init-hydras.el
@@ -6,7 +6,7 @@
   (interactive)
   (find-file (concat user-emacs-directory file-name)))
 
-(defhydra jco/hydra-main-menu (:color blue :hint nil)
+(defhydra jco/hydra-main-menu (:color teal :hint nil)
   "
 Menu: _a_pp _e_dit-cfg _p_kgs _s_woop _S_nippets _v_cs"
   ("a" jco/hydra-app/body)
@@ -16,7 +16,7 @@ Menu: _a_pp _e_dit-cfg _p_kgs _s_woop _S_nippets _v_cs"
   ("S" jco/hydra-snippets/body)
   ("v" jco/hydra-vcs/body))
 
-(defhydra jco/hydra-edit-config (:color blue :hint nil)
+(defhydra jco/hydra-edit-config (:color teal :hint nil)
   "
 Edit cfg: _i_nit _c_ommon _f_ile _h_ydras _p_ackages _t_heme"
   ("i" (open-config-file "init.el"))
@@ -26,34 +26,34 @@ Edit cfg: _i_nit _c_ommon _f_ile _h_ydras _p_ackages _t_heme"
   ("p" (open-config-file "lisp/init-packages.el"))
   ("t" (open-config-file "lisp/init-theme.el")))
 
-(defhydra jco/hydra-packages (:color blue :hint nil)
+(defhydra jco/hydra-packages (:color teal :hint nil)
   "
 Packages: _l_ist _n_o-fetch _u_pgrade-all"
   ("l" list-packages)
   ("n" package-list-packages-no-fetch)
   ("u" package-utils-upgrade-all))
 
-(defhydra jco/hydra-snippets (:color blue :hint nil)
+(defhydra jco/hydra-snippets (:color teal :hint nil)
   "
 Snippets: _i_nsert _e_dit _r_eload"
   ("i" yas/insert-snippet)
   ("e" yas/visit-snippet-file)
   ("r" yas/reload-all))
 
-(defhydra jco/hydra-swoop (:color blue :hint nil)
+(defhydra jco/hydra-swoop (:color teal :hint nil)
   "
 Swoop: _m_ulti multi-_a_ll _s_woop"
   ("m" helm-multi-swoop)
   ("a" helm-multi-swoop-all)
   ("s" helm-swoop))
 
-(defhydra jco/hydra-vcs (:color blue :hint nil)
+(defhydra jco/hydra-vcs (:color teal :hint nil)
   "
 VCS: _g_it _m_ercurial"
   ("g" magit-status)
   ("m" monky-status))
 
-(defhydra jco/hydra-app (:color blue :hint nil)
+(defhydra jco/hydra-app (:color teal :hint nil)
   "
 App: _e_rc _m_u4e"
   ("e" (erc :server "irc.freenode.net" :port 6667))
@@ -61,12 +61,12 @@ App: _e_rc _m_u4e"
 
 ;;; Help menu
 
-(defhydra jco/hydra-help (:color blue :hint nil)
+(defhydra jco/hydra-help (:color teal :hint nil)
   "
 _a_propos"
   ("a" jco/hydra-apropos/body))
 
-(defhydra jco/hydra-apropos (:color blue :hint nil)
+(defhydra jco/hydra-apropos (:color teal :hint nil)
   "
 Apropos: _a_propos _c_md _d_oc _v_al _l_ib _o_ption _v_ar _i_nfo _t_ags"
   ("a" apropos)
