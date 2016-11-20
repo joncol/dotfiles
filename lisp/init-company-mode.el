43cc6d8bc74a6ff5a6f0b36b5f46b13f7cf4b3ea
Jonas Collberg <jonas.collberg@gmail.com>
2016-11-20 16:58:16 +0100

    Rename function

--- lisp/init-company-mode.el
+++ lisp/init-company-mode.el
@@ -1,13 +1,14 @@
 (add-hook 'after-init-hook 'global-company-mode)
 
-(defun on-off-fci-before-company(command)
+(defun turn-off-fci-during-company-complete(command)
   "Fixes the issue where the first item is shown far off to the right."
   (when (string= "show" command)
     (turn-off-fci-mode))
   (when (string= "hide" command)
     (turn-on-fci-mode)))
 
-(advice-add 'company-call-frontends :before #'on-off-fci-before-company)
+(advice-add 'company-call-frontends :before
+            #'turn-off-fci-during-company-complete)
 
 (add-hook 'company-mode-hook
           (lambda ()
