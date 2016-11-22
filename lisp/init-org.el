b96914417cad763ff9ef02d1a4b22c398ee5e6f7
Jonas Collberg <jonas.collberg@orzone.com>
2016-11-22 10:43:52 +0100

    Tweak org settings

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -193,4 +193,35 @@ foreground and background of code, to the current theme's colors"
 
 (define-key org-mode-map (kbd "M-o") 'ace-link-org)
 
+(setq org-hide-emphasis-markers t)
+
+(font-lock-add-keywords
+ 'org-mode
+ '(("^ +\\([-*]\\) "
+    (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
+
+;; (let* ((variable-tuple
+;;         (cond ((x-list-fonts "Source Sans Pro") '(:font "Source Sans Pro"))
+;;               ((x-list-fonts "Lucida Grande")   '(:font "Lucida Grande"))
+;;               ((x-list-fonts "Verdana")         '(:font "Verdana"))
+;;               ((x-family-fonts "Sans Serif")    '(:family "Sans Serif"))
+;;               (nil (warn "Cannot find a Sans Serif Font."))))
+;;        (base-font-color (face-foreground 'default nil 'default))
+;;        ;; (headline `(:inherit default :weight bold :foreground ,base-font-color))
+;;        (headline `(:inherit default :weight bold)))
+
+;;   (custom-theme-set-faces
+;;    'user
+;;    `(org-level-8 ((t (,@headline ,@variable-tuple))))
+;;    `(org-level-7 ((t (,@headline ,@variable-tuple))))
+;;    `(org-level-6 ((t (,@headline ,@variable-tuple))))
+;;    `(org-level-5 ((t (,@headline ,@variable-tuple))))
+;;    `(org-level-4 ((t (,@headline ,@variable-tuple :height 1.1))))
+;;    `(org-level-3 ((t (,@headline ,@variable-tuple :height 1.25))))
+;;    `(org-level-2 ((t (,@headline ,@variable-tuple :height 1.5))))
+;;    `(org-level-1 ((t (,@headline ,@variable-tuple :height 1.75))))
+;;    `(org-document-title ((t (,@headline ,@variable-tuple
+;;                                         :height 1.5
+;;                                         :underline nil))))))
+
 (provide 'init-org)
