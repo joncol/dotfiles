8580d7f53eb168d2b5039933b08c6b21177e124d
Jonas Collberg <jonas.collberg@gmail.com>
2016-11-24 19:30:06 +0100

    Fix config bugs

--- lisp/init-common.el
+++ lisp/init-common.el
@@ -157,10 +157,10 @@
 (use-package avy
   :config
   (eval-after-load "evil"
-    (progn (evil-leader/set-key "f" 'evil-avy-goto-char)
-           (evil-leader/set-key "#" 'evil-avy-goto-line)
-           (evil-leader/set-key "F" 'evil-avy-goto-word-or-subword-1)
-           (avy-setup-default)))
+    '(progn (evil-leader/set-key "f" 'evil-avy-goto-char)
+            (evil-leader/set-key "#" 'evil-avy-goto-line)
+            (evil-leader/set-key "F" 'evil-avy-goto-word-or-subword-1)
+            (avy-setup-default)))
 
   (setq avy-case-fold-search nil))
 
