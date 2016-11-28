7d5176cf01dbf06d63772e3d7c18d4fc047e75a5
Jonas Collberg <jonas.collberg@gmail.com>
2016-11-28 22:46:18 +0100

    Remove unnecessary quotes from init-theme.el

--- lisp/init-theme.el
+++ lisp/init-theme.el
@@ -1,47 +1,57 @@
+;;; #init-theme.el --- Theme settings -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
 ;;; Change this to whatever theme you want.
-(setq jco/theme 'sanityinc-tomorrow-blue)
+(defvar jco/theme)
+(set (make-local-variable 'jco/theme) 'sanityinc-tomorrow-blue)
 
 (require 'spaceline-config)
 (spaceline-spacemacs-theme)
 (spaceline-helm-mode)
 (spaceline-info-mode)
 
-(setq jco/cursor-color "green")
+(defvar jco/cursor-color)
+(set (make-local-variable 'jco/cursor-color) "green")
 
 (load-theme jco/theme t)
 
-(case jco/theme
-  ('gotham
+(cl-case jco/theme
+  (gotham
    (setq jco/cursor-color "lightblue")
    (set-face-background 'evil-search-highlight-persist-highlight-face
                         "RoyalBlue4"))
 
-  ('material
+  (material
    (set-face-background 'hl-line "#37474f")
    (set-face-background 'org-todo nil))
 
-  ('material-light
+  (material-light
    (setq jco/cursor-color "azure4")
    (set-face-background 'hl-line "Gray75"))
 
-  ('meacupla
+  (meacupla
    (setq jco/cursor-color "azure4")
    (set-face-background 'hl-line "Gray80")
    (set-face-background 'whitespace-trailing "IndianRed1"))
 
-  ('minimal
+  (minimal
    (set-face-background 'hl-line "#101f24")
    (set-face-background 'org-todo nil)
    (set-face-background 'evil-search-highlight-persist-highlight-face
                         "DarkOrange4"))
 
-  ('molokai
+  (molokai
    (set-face-foreground 'font-lock-comment-face "azure4")
    (set-face-background
     'evil-search-highlight-persist-highlight-face "RoyalBlue4")
    )
 
-  ('organic-green
+  (organic-green
    (setq jco/cursor-color "gray25")
    (set-face-background 'helm-selection "#a0f0a0")
    (set-face-background 'show-paren-match "#c0c060"))
@@ -52,7 +62,7 @@
    (set-face-background 'evil-search-highlight-persist-highlight-face
                         "RoyalBlue"))
 
-  ('sanityinc-tomorrow-blue
+  (sanityinc-tomorrow-blue
    (setq jco/cursor-color "snow")
    (set-face-background 'evil-search-highlight-persist-highlight-face
                         "RoyalBlue")
@@ -61,14 +71,14 @@
    (set-face-foreground 'company-tooltip-common-selection "VioletRed4")
    (set-face-background 'company-scrollbar-fg "LightBlue"))
 
-  ('soothe
+  (soothe
    (require 'linum)
    (set-face-foreground 'linum "gray35"))
 
-  ('tao-yang
+  (tao-yang
    (setq jco/cursor-color "azure4"))
 
-  ('tao-yin
+  (tao-yin
    (setq jco/cursor-color "gray50")
    (set-face-background 'hl-line "gray16")
    (set-face-foreground 'linum "gray25")
@@ -78,8 +88,11 @@
 (set-face-foreground 'minibuffer-prompt "#263238")
 (set-face-background 'minibuffer-prompt "#afd700")
 
+(require 'evil)
 (setq evil-normal-state-cursor `(,jco/cursor-color box))
 (setq evil-insert-state-cursor `(,jco/cursor-color bar))
 (blink-cursor-mode -1)
 
 (provide 'init-theme)
+
+;;; init-theme.el ends here
