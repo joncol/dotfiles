d37778c71120eebec85b68273fafe7f066cc14f8
Jonas Collberg <jonas.collberg@gmail.com>
2016-12-02 20:59:56 +0100

    Do not treat "-" as word separator in Lisp modes

--- lisp/init-lisp-common.el
+++ lisp/init-lisp-common.el
@@ -1,9 +1,26 @@
+;;; #init-lisp-common.el --- All Lisp modes -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
 (defun init-lisp-common ()
+  "Common configuration options for all Lisp modes."
+
+  ;; do not treat "-" as a word separator
+  (modify-syntax-entry ?- "w")
   (paredit-mode)
   (evil-paredit-mode)
   (diminish 'paredit-mode)
-  (setq redshank-prefix-key "C-c C-r")
-  (redshank-mode)
-  (diminish 'redshank-mode))
+  (use-package redshank
+    :demand
+    :diminish
+    :config
+    (redshank-mode)
+    (setq redshank-prefix-key "C-c C-r")))
 
 (provide 'init-lisp-common)
+
+;;; init-lisp-common.el ends here
