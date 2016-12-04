4be8a991aee3a24671eed33c40de367bcb545eda
Jonas Collberg <jonas.collberg@gmail.com>
2016-12-04 01:42:34 +0100

    Make init-lua.el use use-package

--- lisp/init-lua.el
+++ lisp/init-lua.el
@@ -1,7 +1,23 @@
-(setq lua-indent-level 2)
+;;; #init-lua.el --- Lua config -*- lexical-binding: t; -*-
 
-(add-hook 'lua-mode-hook
-          (lambda ()
-            (setq evil-shift-width 2)))
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(use-package company-lua
+  :defer t
+  :commands lua-mode)
+
+(use-package lua-mode
+  :defer t
+  :commands lua-mode
+  :config
+  (setq evil-shift-width 2)
+  (setq lua-indent-level 2)
+  (add-to-list 'company-backends 'company-lua))
 
 (provide 'init-lua)
+
+;;; init-lua.el ends here
