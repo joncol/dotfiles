6fbe41c4662ae39fdcdfe5e0ea981a498dbe12cd
Jonas Collberg <jonas.collberg@gmail.com>
2016-12-15 00:05:06 +0100

    Add elpy and jedi for Python development

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -1,3 +1,39 @@
-(setq python-shell-prompt-detect-failure-warning nil)
+;;; #init-python.el --- Python config -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(with-eval-after-load 'python
+  (setq python-shell-prompt-detect-failure-warning nil))
+
+(use-package elpy
+  ;; :defer 2
+  :diminish elpy-mode
+  :diminish highlight-indentation-mode
+
+  :init
+  (when (require 'flycheck nil t)
+    (remove-hook 'elpy-modules 'elpy-module-flymake)
+    (remove-hook 'elpy-modules 'elpy-module-yasnippet)
+    (remove-hook 'elpy-mode-hook 'elpy-module-highlight-indentation))
+
+  (elpy-enable)
+
+  :config
+  (setq elpy-rpc-backend "jedi"))
+
+(use-package company-jedi
+  :init
+  (with-eval-after-load 'python
+    (add-to-list 'company-backends '(company-jedi company-files))))
+
+(require 'jedi-core)
+
+(add-hook 'python-mode-hook #'jedi-mode)
 
 (provide 'init-python)
+
+;;; init-python.el ends here
