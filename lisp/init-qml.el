8edee76dc2a2d5d4359788b2b2629695d107d59c
Jonas Collberg <jonas.collberg@gmail.com>
2016-12-23 18:20:53 +0100

    Use use-package for qml-mode

--- lisp/init-qml.el
+++ lisp/init-qml.el
@@ -8,13 +8,14 @@
 
 (add-to-list 'auto-mode-alist '("\\.qml$" . qml-mode))
 
-(add-hook 'qml-mode-hook
-          (lambda ()
-            (set (make-local-variable 'compile-command)
-                 (jco/cmake-compile-command))
-            (jco/define-bindings qml-mode-map '(("<f6>" . compile)))
-            (setq comment-start "//"
-                  comment-end "")))
+(use-package qml-mode
+  :bind (:map qml-mode-map
+              ([f6] . compile))
+  :config
+  (set (make-local-variable 'compile-command)
+       (jco/cmake-compile-command))
+  (setq comment-start "//"
+        comment-end ""))
 
 (provide 'init-qml)
 
