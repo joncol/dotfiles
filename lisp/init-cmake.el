eec9c3d69233b729bf7a47acc2b18f0fa1e263cb
Jonas Collberg <jonas.collberg@gmail.com>
2017-03-22 14:47:41 +0100

    Update CMake configuration

--- lisp/init-cmake.el
+++ lisp/init-cmake.el
@@ -1,5 +1,17 @@
-(add-to-list 'auto-mode-alist '("CMakeLists\\.txt\\'" . cmake-mode))
+;;; #init-cmake.el --- CMake configuration -*- lexical-binding: t; -*-
 
-(setq cmake-tab-width 4)
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(use-package cmake-mode
+  :init
+  (add-to-list 'auto-mode-alist '("CMakeLists\\.txt\\'" . cmake-mode))
+  :config
+  (setq cmake-tab-width 4))
 
 (provide 'init-cmake)
+
+;;; init-cmake.el ends here
