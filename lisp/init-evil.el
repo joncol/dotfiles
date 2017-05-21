ea2a6095b28cc641751fc1b44c4fffeff458de31
Jonas Collberg <jonas.collberg@gmail.com>
2017-05-21 13:43:30 +0200

    Move evil-leader initialization to init-evil.el

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -26,6 +26,13 @@
            (t (setq unread-command-events (append unread-command-events
                                                   (list evt)))))))))
 
+(use-package evil-leader
+  :init
+  ;; Enable global-evil-leader-mode before evil-mode, to make leader key work
+  ;; in *Messages* and *scratch* buffers.
+  (global-evil-leader-mode)
+  (evil-leader/set-leader ","))
+
 (use-package evil
   :init
   (evil-mode)
