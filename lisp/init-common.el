cad10c47d124ac91cd144ac7140aa4b2d551b9d1
Jonas Collberg <jonas.collberg@gmail.com>
2017-05-23 13:35:26 +0200

    Add package dashboard

--- lisp/init-common.el
+++ lisp/init-common.el
@@ -122,6 +122,17 @@
   :init
   (counsel-projectile-on))
 
+(use-package dashboard
+  :config
+  (dashboard-setup-startup-hook)
+  (setq dashboard-items '((recents . 5)
+                          (bookmarks . 5)
+                          (projects . 5)
+                          (agenda . 5)))
+
+  (setq dashboard-banner-logo-title "Emacs Master Race")
+  (setq dashboard-startup-banner 'logo))
+
 (use-package ecb
   :config
   (setq ecb-tip-of-the-day nil))
