6de2741038ca76e66406b3e94ec05091fc788d30
Jonas Collberg <jonas.collberg@gmail.com>
2017-05-23 16:18:09 +0200

    Modify dashboard config slightly

--- lisp/init-common.el
+++ lisp/init-common.el
@@ -123,15 +123,17 @@
   (counsel-projectile-on))
 
 (use-package dashboard
+  :diminish dashboard-mode
+
   :config
-  (dashboard-setup-startup-hook)
   (setq dashboard-items '((recents . 5)
                           (bookmarks . 5)
                           (projects . 5)
                           (agenda . 5)))
 
   (setq dashboard-banner-logo-title "Emacs Master Race")
-  (setq dashboard-startup-banner 'logo))
+  (setq dashboard-startup-banner 'logo)
+  (dashboard-setup-startup-hook))
 
 (use-package ecb
   :config
