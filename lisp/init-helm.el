8dcbff650ae363a21dba4ab074856dca615458d8
Jonas Collberg <jonas.collberg@gmail.com>
2017-05-30 12:21:42 +0200

    Remove comment

--- lisp/init-helm.el
+++ lisp/init-helm.el
@@ -5,9 +5,6 @@
 
 ;;; Code:
 
-;; The default "C-x c" is quite close to "C-x C-c", which quits Emacs.
-;; Changed to "C-c h". Note: We must set "C-c h" globally, because we
-;; cannot change `helm-command-prefix-key' once `helm-config' is loaded.
 (use-package helm
   :init
   (helm-mode)
