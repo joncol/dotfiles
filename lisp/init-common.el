bb48627c65a7394be9065e36d52df1cbfa123ba8
Jonas Collberg <jonas.collberg@gmail.com>
2017-06-12 14:13:24 +0200

    Add binding for avy-goto-char-timer

--- lisp/init-common.el
+++ lisp/init-common.el
@@ -78,6 +78,7 @@
   (evil-leader/set-key "f" 'evil-avy-goto-char)
   (evil-leader/set-key "#" 'evil-avy-goto-line)
   (evil-leader/set-key "F" 'evil-avy-goto-word-or-subword-1)
+  (evil-leader/set-key "/" 'avy-goto-char-timer)
   (avy-setup-default)
 
   (setq avy-case-fold-search nil))
