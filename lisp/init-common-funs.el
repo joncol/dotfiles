17416cb5cfc9b69971e1f91f9d758d23ac65e596
Jonas Collberg <jonas.collberg@gmail.com>
2017-06-14 12:22:37 +0200

    More formatting of whitespace around braces

--- lisp/init-common-funs.el
+++ lisp/init-common-funs.el
@@ -226,12 +226,25 @@ CMakeLists.txt file."
       (replace-match "\n\n"))))
 
 (defun jco/spaces-before-braces ()
-  "Add a space (if needed) before left braces."
+  "Fix formatting of braces.
+Add a space (if needed) before left braces, and remove empty lines after opening
+brace and before closing brace."
   (interactive)
   (save-excursion
+    ;; Single space before opening brace.
     (goto-char (point-min))
     (while (re-search-forward "\\([^ ]\\)\\s-*{" nil t)
-      (replace-match "\\1 {"))))
+      (replace-match "\\1 {"))
+
+    ;; Remove empty line(s) after opening brace.
+    (goto-char (point-min))
+    (while (re-search-forward "{\n\n+" nil t)
+      (replace-match "{\n"))
+
+    ;; Remove empty line(s) before closing brace.
+    (goto-char (point-min))
+    (while (re-search-forward "\n\n+\\(\\s-*\\)}" nil t)
+      (replace-match "\n\\1}"))))
 
 (provide 'init-common-funs)
 
