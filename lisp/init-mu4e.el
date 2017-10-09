ffcb89edb49346542a82724df9aefd7a2ff742ca
Jonas Collberg <jonas.collberg@zimpler.com>
2017-10-09 12:22:31 +0200

    Move mu4e config to mode hook

--- lisp/init-mu4e.el
+++ lisp/init-mu4e.el
@@ -9,82 +9,6 @@
 (when (and (not (eq system-type 'windows-nt))
            (not (string-equal system-name "joule-Broxton-M"))
            (not (string-equal system-name "jco")))
-  (add-to-list 'load-path "/usr/share/emacs/site-lisp/mu4e")
-
-  (require 'mu4e)
-  (require 'imapfilter)
-
-  (setq mu4e-get-mail-command "offlineimap")
-  (setq mu4e-update-interval 120)
-  (setq mu4e-sent-messages-behavior 'sent)
-  (setq mu4e-html2text-command "w3m -T text/html")
-  (setq mu4e-view-show-images t)
-  (setq mu4e-view-show-addresses t)
-  (add-to-list 'mu4e-view-actions '("ViewInBrowser" .
-                                    mu4e-action-view-in-browser) t)
-  (setq mu4e-view-show-addresses t)
-
-  (setq mu4e-compose-context-policy 'always-ask)
-  (setq mu4e-compose-in-new-frame t)
-  (setq mu4e-save-multiple-attachments-without-asking t)
-  (setq mu4e-compose-format-flowed t)
-  (setq mu4e-compose-dont-reply-to-self t)
-
-  (setq mu4e-headers-date-format "%Y-%m-%d %H:%M")
-  (setq mu4e-headers-fields
-        '((:date    . 25)
-          (:flags   .  6)
-          (:from    . 22)
-          (:subject . nil)))
-
-  (defun compl-fun (prompt maildirs predicate require-match initial-input)
-    (helm-comp-read prompt maildirs
-                    :name prompt
-                    :must-match t))
-
-  (defun jco/smtp-server ()
-    (cond ((or (s-contains? "gmail.com" user-mail-address)
-               (s-contains? "zimpler.com" user-mail-address)) "smtp.gmail.com")
-          ((s-contains? "kolab" user-mail-address) "smtp.kolabnow.com")))
-
-  (defun jco/my-send-it ()
-    (setq smtpmail-starttls-credentials `((,(jco/smtp-server) 587 nil nil))
-          smtpmail-auth-credentials
-          `((,(jco/smtp-server) 587 user-mail-address nil))
-          smtpmail-default-smtp-server (jco/smtp-server)
-          smtpmail-smtp-server (jco/smtp-server))
-    (smtpmail-send-it))
-
-  (require 'smtpmail)
-
-  (setq message-send-mail-function 'jco/my-send-it
-        starttls-use-gnutls t
-        smtpmail-smtp-service 587)
-
-  ;; don't keep message buffers around
-  (setq message-kill-buffer-on-exit t)
-
-  (add-hook 'mu4e-update-pre-hook
-            #'imapfilter)
-
-  (add-hook 'mu4e-view-mode-hook
-            (lambda ()
-              (mu4e-view-fill-long-lines)))
-
-  (add-hook 'mu4e-compose-mode-hook
-            (lambda ()
-              (fci-mode)
-              (ethan-wspace-mode -1)
-              (turn-off-auto-fill)
-              (footnote-mode)
-              (setq truncate-lines nil)
-              (setq word-wrap t)))
-
-  (setq mu4e-org-contacts-file "~/.contacts")
-  (add-to-list 'mu4e-headers-actions
-               '("org-contact-add" . mu4e-action-add-org-contact) t)
-  (add-to-list 'mu4e-view-actions
-               '("org-contact-add" . mu4e-action-add-org-contact) t)
 
   ;; Useful for searching contacts.
   (use-package helm-mu)
@@ -106,9 +30,9 @@
                      (mu4e-sent-folder . "/personal_mykolab/Sent")
                      (mu4e-trash-folder . "/personal_mykolab/Trash")
                      (mu4e-maildir-shortcuts .
-                      (("/personal_mykolab/INBOX" . ?i)
-                       ("/personal_mykolab/Sent" . ?s)
-                       ("/personal_mykolab/Trash" . ?t)))
+                                             (("/personal_mykolab/INBOX" . ?i)
+                                              ("/personal_mykolab/Sent" . ?s)
+                                              ("/personal_mykolab/Trash" . ?t)))
                      (mu4e-completing-read-function . compl-fun)
                      ))
            ,(make-mu4e-context
@@ -129,12 +53,90 @@
                      (mu4e-sent-folder . "/zimpler_gmail/[Gmail].Sent Mail")
                      (mu4e-trash-folder . "/zimpler_gmail/[Gmail].Trash")
                      (mu4e-maildir-shortcuts .
-                      (("/zimpler_gmail/INBOX" . ?i)
-                       ("/zimpler_gmail/[Gmail].Sent Mail" . ?s)
-                       ("/zimpler_gmail/[Gmail].Trash" . ?t)
-                       ("/zimpler_gmail/[Gmail].All Mail" . ?a)))
-                     (mu4e-completing-read-function . compl-fun)
-                     )))))
+                                             (("/zimpler_gmail/INBOX" . ?i)
+                                              ("/zimpler_gmail/[Gmail].Sent Mail" . ?s)
+                                              ("/zimpler_gmail/[Gmail].Trash" . ?t)
+                                              ("/zimpler_gmail/[Gmail].All Mail" . ?a)))
+                     (mu4e-completing-read-function . compl-fun)))))
+
+  (add-hook 'mu4e-main-mode-hook
+            (lambda ()
+              (add-to-list 'load-path "/usr/share/emacs/site-lisp/mu4e")
+
+              (require 'mu4e)
+              (require 'imapfilter)
+
+              (setq mu4e-get-mail-command "offlineimap")
+              (setq mu4e-update-interval 120)
+              (setq mu4e-sent-messages-behavior 'sent)
+              (setq mu4e-html2text-command "w3m -T text/html")
+              (setq mu4e-view-show-images t)
+              (setq mu4e-view-show-addresses t)
+              (add-to-list 'mu4e-view-actions '("ViewInBrowser" .
+                                                mu4e-action-view-in-browser) t)
+              (setq mu4e-view-show-addresses t)
+
+              (setq mu4e-compose-context-policy 'always-ask)
+              (setq mu4e-compose-in-new-frame t)
+              (setq mu4e-save-multiple-attachments-without-asking t)
+              (setq mu4e-compose-format-flowed t)
+              (setq mu4e-compose-dont-reply-to-self t)
+
+              (setq mu4e-headers-date-format "%Y-%m-%d %H:%M")
+              (setq mu4e-headers-fields
+                    '((:date    . 25)
+                      (:flags   .  6)
+                      (:from    . 22)
+                      (:subject . nil)))
+
+              (defun compl-fun (prompt maildirs predicate require-match initial-input)
+                (helm-comp-read prompt maildirs
+                                :name prompt
+                                :must-match t))
+
+              (defun jco/smtp-server ()
+                (cond ((or (s-contains? "gmail.com" user-mail-address)
+                           (s-contains? "zimpler.com" user-mail-address)) "smtp.gmail.com")
+                      ((s-contains? "kolab" user-mail-address) "smtp.kolabnow.com")))
+
+              (defun jco/my-send-it ()
+                (setq smtpmail-starttls-credentials `((,(jco/smtp-server) 587 nil nil))
+                      smtpmail-auth-credentials
+                      `((,(jco/smtp-server) 587 user-mail-address nil))
+                      smtpmail-default-smtp-server (jco/smtp-server)
+                      smtpmail-smtp-server (jco/smtp-server))
+                (smtpmail-send-it))
+
+              (require 'smtpmail)
+
+              (setq message-send-mail-function 'jco/my-send-it
+                    starttls-use-gnutls t
+                    smtpmail-smtp-service 587)
+
+              ;; don't keep message buffers around
+              (setq message-kill-buffer-on-exit t)
+
+              (setq mu4e-org-contacts-file "~/.contacts")
+              (add-to-list 'mu4e-headers-actions
+                           '("org-contact-add" . mu4e-action-add-org-contact) t)
+              (add-to-list 'mu4e-view-actions
+                           '("org-contact-add" . mu4e-action-add-org-contact) t))))
+
+  (add-hook 'mu4e-update-pre-hook
+            #'imapfilter)
+
+  (add-hook 'mu4e-view-mode-hook
+            (lambda ()
+              (mu4e-view-fill-long-lines)))
+
+  (add-hook 'mu4e-compose-mode-hook
+            (lambda ()
+              (fci-mode)
+              (ethan-wspace-mode -1)
+              (turn-off-auto-fill)
+              (footnote-mode)
+              (setq truncate-lines nil)
+              (setq word-wrap t)))
 
 (provide 'init-mu4e)
 
