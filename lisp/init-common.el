2a12d50ac2862ac1eadea8f64992404291803a3f
Jonas Collberg <jonas.collberg@zimpler.com>
2017-11-03 10:07:30 +0100

    Disable `' pairs in Clojure modes

--- lisp/init-common.el
+++ lisp/init-common.el
@@ -394,7 +394,7 @@
                          ("C-M-a" . sp-backward-down-sexp)
                          ("C-S-d" . sp-beginning-of-sexp)
                          ("C-S-a" . sp-end-of-sexp)))
-  (sp-with-modes sp--lisp-modes
+  (sp-with-modes (set-difference sp--lisp-modes sp-clojure-modes)
     (sp-local-pair "'" nil :actions nil)
     (sp-local-pair "`" "'" :when '(sp-in-string-p sp-in-comment-p))
     (sp-local-pair "`" nil
