f0314f2618929b27ed4ff229d75fb5371ae8bcfc
Jonas Collberg <jonas.collberg@gmail.com>
2017-11-22 21:47:12 +0100

    Turn off fci-mode while running shell commands

--- lisp/init-fci.el
+++ lisp/init-fci.el
@@ -1,3 +1,11 @@
+;;; #init-fci.el --- Config for fci-mode -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
 (use-package fill-column-indicator
   :config
   (setq-default fill-column 80)
@@ -9,8 +17,10 @@
 
 (defvar modi/htmlize-initial-fci-state nil
   "Variable to store the state of `fci-mode' when `htmlize-buffer' is called.")
+
 (defvar modi/htmlize-initial-flyspell-state nil
-  "Variable to store the state of `flyspell-mode' when `htmlize-buffer' is called.")
+  "Variable to store the state of `flyspell-mode' when `htmlize-buffer' is
+called.")
 
 (defun modi/htmlize-before-hook-fn ()
   (when (fboundp 'fci-mode)
@@ -21,6 +31,7 @@
     (setq modi/htmlize-initial-flyspell-state flyspell-mode)
     (when flyspell-mode
       (flyspell-mode -1))))
+
 (add-hook 'htmlize-before-hook #'modi/htmlize-before-hook-fn)
 
 (defun modi/htmlize-after-hook-fn ()
@@ -30,6 +41,23 @@
   (when (fboundp 'flyspell-mode)
     (when modi/htmlize-initial-flyspell-state
       (flyspell-mode 1))))
+
 (add-hook 'htmlize-after-hook #'modi/htmlize-after-hook-fn)
 
+(defun disable-fci-temporarily (orig-fun &rest args)
+  "Disable `fci-mode' before calling ORIG-FUN; re-enable afterwards."
+  (let ((fci-was-initially-on (when fci-mode
+                                (prog1
+                                    fci-mode
+                                  (fci-mode -1)))))
+    (prog1
+        (apply orig-fun args)
+      (when fci-was-initially-on
+        (fci-mode 1)))))
+
+(advice-add 'shell-command :around #'disable-fci-temporarily)
+(advice-add 'shell-command-on-region :around #'disable-fci-temporarily)
+
 (provide 'init-fci)
+
+;;; init-fci.el ends here
