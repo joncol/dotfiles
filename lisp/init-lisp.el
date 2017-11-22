7f6dbd626cf2693c9a5c98e483c660dac05d6b4b
Jonas Collberg <jonas.collberg@gmail.com>
2017-11-22 22:03:29 +0100

    Fix Common Lisp config

--- lisp/init-lisp.el
+++ lisp/init-lisp.el
@@ -1,12 +1,26 @@
+;;; #init-lisp.el --- Common Lisp config -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
 (require 'init-lisp-common)
 
+(use-package slime
+  :after lisp-mode)
+
 (add-hook 'lisp-mode-hook
-          (lambda ()
-            (init-lisp-common)
-            (setq inferior-lisp-program "sbcl")
-            (bind-key (kbd "M-.") 'slime-edit-definition lisp-mode-map)))
+          #'(lambda ()
+              (init-lisp-common)
+              (setq inferior-lisp-program "sbcl")
+              (setq slime-contribs '(slime-fancy))
+              (bind-key (kbd "M-.") 'slime-edit-definition lisp-mode-map)))
 
 (when (file-exists-p "~/quicklisp/slime-helper.el")
   (load (expand-file-name "~/quicklisp/slime-helper.el")))
 
 (provide 'init-lisp)
+
+;;; init-lisp.el ends here
