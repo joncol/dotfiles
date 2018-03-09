79f0592d57fd72198db2f962a004bf3b17a5b30e
Jonas Collberg <jonas.collberg@zimpler.com>
2018-03-09 09:50:42 +0100

    Add lein/profiles.clj

---
+++ lein/profiles.clj
@@ -0,0 +1,12 @@
+{:user {:dependencies [[org.clojure/clojure "1.9.0"]
+                       [org.clojure/tools.trace "0.7.9"]
+                       [spyscope "0.1.7-SNAPSHOT"]]
+        :injections [(require 'spyscope.core)
+                     (use 'clojure.repl)
+                     (use 'clojure.tools.trace)
+                     (use 'no.disassemble)]
+        :plugins [[lein-nodisassemble "0.1.3"]
+                  [lein-ancient "0.6.15"]
+                  [lein-cljfmt "0.5.7"]
+                  [lein-kibit "0.1.6"]
+                  [venantius/ultra "0.5.2"]]}}
