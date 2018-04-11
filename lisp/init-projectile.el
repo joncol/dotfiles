66428c2407f66eaac925cd538aaee36793613a83
Jonas Collberg <jonas.collberg@gmail.com>
2018-04-11 16:47:34 +0200

    Remove references to `helm-mode` from `init-projectile.el`

--- lisp/init-projectile.el
+++ lisp/init-projectile.el
@@ -11,9 +11,6 @@
   (projectile-mode)
 
   :config
-  (when helm-mode
-    (define-key projectile-command-map (kbd "s a") #'helm-ag-project-root)
-    (setq projectile-completion-system 'helm))
   (when (not (eq system-type 'windows-nt))
     (setq projectile-indexing-method 'native))
   (setq projectile-enable-caching t)
@@ -47,11 +44,6 @@
       (cider-jack-in)))
 
   :diminish projectile-mode)
-
-(use-package helm-projectile
-  :if helm-mode
-  :init
-  (helm-projectile-on))
 
 (provide 'init-projectile)
 
