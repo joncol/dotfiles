390703062fcbc1c05ea04a4a66ca27fecf94c179
Jonas Collberg <jonas.collberg@gmail.com>
2018-04-16 17:17:44 +0200

    Add boilerplate comment to `init-emacs-lisp.el`

--- lisp/init-emacs-lisp.el
+++ lisp/init-emacs-lisp.el
@@ -1,5 +1,15 @@
-(require 'init-lisp-common)
+;;; #init-emacs-lisp.el --- Emacs Lisp configuration -*- lexical-binding: t; -*-
 
-(add-hook 'emacs-lisp-mode-hook 'init-lisp-common)
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(add-hook 'emacs-lisp-mode-hook
+          (lambda ()
+            (init-lisp-common)))
 
 (provide 'init-emacs-lisp)
+
+;;; init-emacs-lisp.el ends here
