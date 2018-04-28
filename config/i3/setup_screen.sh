8d45bc3a929d2abbbb9ab0c31f14538758db2809
Jonas Collberg <jonas.collberg@gmail.com>
2018-04-28 17:39:32 +0200

    Add support for 2 home screens

--- config/i3/setup_screen.sh
+++ config/i3/setup_screen.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
 
-xrandr | grep "DP1-1 connected "
+xrandr | grep "^DP1-1 connected "
 if [ $? -eq 0 ]; then
     echo Found work screen
     xrandr --output DP1-1 --auto --right-of eDP1
     exit 0
 fi
 
-xrandr | grep "HDMI2 connected "
+xrandr | grep "^DP1 connected "
 if [ $? -eq 0 ]; then
-    echo Found home screen
-    xrandr --output HDMI2 --auto --right-of eDP1
-    exit 0
+    echo Found left home screen
+    xrandr --output DP1 --auto --right-of eDP1
+    xrandr --output eDP1 --off
+fi
+
+xrandr | grep "^HDMI2 connected "
+if [ $? -eq 0 ]; then
+    echo Found right home screen
+    xrandr --output HDMI2 --auto --right-of DP1
 fi
