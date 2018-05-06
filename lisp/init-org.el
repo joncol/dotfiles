3e509a6e1b7f003164e22d6b43958ca8f3e0562d
Jonas Collberg <jonas.collberg@gmail.com>
2018-05-06 15:46:58 +0200

    Add binding for opening `reading.org`

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -236,6 +236,7 @@ PRIORITY may be one of the characters ?A, ?B, or ?C."
 
 (evil-leader/set-key "o g" (jco/find-org-file "gtd.org"))
 (evil-leader/set-key "o n" (jco/find-org-file "notes.org"))
+(evil-leader/set-key "o r" (jco/find-org-file "reading.org"))
 (evil-leader/set-key "o w" (jco/find-org-file "work.org"))
 (evil-leader/set-key "o p" (jco/find-org-file "todo.org"
                                               (projectile-project-root)))
