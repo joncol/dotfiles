d9b4cab2a54cef39b0a7c50327f27e65f497c029
Jonas Collberg <jonas.collberg@gmail.com>
2018-05-19 20:17:20 +0200

    Fix screen setup script for docking station

--- config/i3/setup_screen.sh
+++ config/i3/setup_screen.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 
-xrandr | grep "^DP1-1 connected "
+xrandr | grep "^DP1-1 connected 3840x2160"
 if [ $? -eq 0 ]; then
     echo Found work screen
     xrandr --output DP1-1 --auto --right-of eDP1
     exit 0
 fi
 
-xrandr | grep "^DP1 connected "
+xrandr | grep "^HDMI2 connected "
 if [ $? -eq 0 ]; then
     echo Found left home screen
-    xrandr --output DP1 --auto --right-of eDP1
+    xrandr --output HDMI2 --mode 1920x1200 --right-of eDP1
     xrandr --output eDP1 --off
 fi
 
-xrandr | grep "^HDMI2 connected "
+xrandr | grep "^DP1-1 connected"
 if [ $? -eq 0 ]; then
     echo Found right home screen
-    xrandr --output HDMI2 --auto --right-of DP1
+    xrandr --output DP1-1 --mode 1920x1200 --right-of HDMI2
 fi
