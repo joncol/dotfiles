99500207d5180190fec86b71e2068ef8da5bb6d7
Jonas Collberg <jonas.collberg@gmail.com>
2018-05-21 09:47:05 +0200

    Modify `setup_screen.sh` to detect work screen

--- config/i3/setup_screen.sh
+++ config/i3/setup_screen.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
-xrandr | grep "^DP1-1 connected 3840x2160"
+xrandr | grep -zoq -A1 "DP1-1 connected.*3840x2160"
 if [ $? -eq 0 ]; then
     echo Found work screen
     xrandr --output DP1-1 --auto --right-of eDP1
     exit 0
 fi
 
-xrandr | grep "^HDMI2 connected "
+xrandr | grep -q "^HDMI2 connected "
 if [ $? -eq 0 ]; then
     echo Found left home screen
     xrandr --output HDMI2 --mode 1920x1200 --right-of eDP1
     xrandr --output eDP1 --off
 fi
 
-xrandr | grep "^DP1-1 connected"
+xrandr | grep -q "^DP1-1 connected"
 if [ $? -eq 0 ]; then
     echo Found right home screen
     xrandr --output DP1-1 --mode 1920x1200 --right-of HDMI2
