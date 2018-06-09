8383ee554616447b7d7f76a60b0b182c46d447c9
Jonas Collberg <jonas.collberg@gmail.com>
2018-06-09 18:02:58 +0200

    fish: Customize colors

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -23,3 +23,11 @@ end
 
 eval (direnv hook fish)
 thefuck --alias | source
+
+set -x FZF_DEFAULT_OPTS '--bind ctrl-f:page-down,ctrl-b:page-up
+--color fg:124,hl:202,fg+:214,bg+:52,hl+:231
+--color info:52,prompt:196,spinner:208,pointer:196,marker:208
+'
+
+set -x fish_pager_color_prefix '444'
+set -x fish_color_search_match --background='eee'
