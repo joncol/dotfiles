e395c43c86515c325cf1af856e5100181bd14ca4
Jonas Collberg <jonas.collberg@gmail.com>
2018-06-16 23:14:06 +0200

    mail: Remove `offlineimap` configuration

--- homedir/mail/.offlineimap.py
+++
@@ -1,7 +0,0 @@
-import os
-import subprocess
-
-def mailpasswd(account):
-    path = "%s/.passwd/%s.gpg" % (os.environ["HOME"], account)
-    return subprocess.check_output(["gpg2", "--quiet", "--batch", "-d",
-                                    path]).strip()
