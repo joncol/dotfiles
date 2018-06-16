fe42c1a4af03a2b0d44ce0ad5676f35e84caa46e
Jonas Collberg <jonas.collberg@gmail.com>
2018-06-16 17:01:59 +0200

    i3: Fix for Arch Linux
    New screen names.

--- homedir/i3/.config/i3/setup_screen.sh
+++ homedir/i3/.config/i3/setup_screen.sh
@@ -6,22 +6,22 @@ H=$(echo $DIM | sed -r 's/^[0-9]+x([0-9]+).*$/\1/')
 printf "#define WIDTH $W\n#define HEIGHT $H\n" > ~/.dimensions.h
 xrdb ~/.Xresources
 
-xrandr | grep -zoq -A1 "DP1-1 connected.*3840x2160"
+xrandr | grep -zoq -A1 "DP-1-1 connected.*3840x2160"
 if [ $? -eq 0 ]; then
     echo Found work screen
-    xrandr --output DP1-1 --auto --right-of eDP1
+    xrandr --output DP-1-1 --auto --right-of eDP-1
     exit 0
 fi
 
-xrandr | grep -q "^HDMI2 connected "
+xrandr | grep -q "^HDMI-2 connected "
 if [ $? -eq 0 ]; then
     echo Found left home screen
-    xrandr --output HDMI2 --mode 1920x1200 --right-of eDP1
-    xrandr --output eDP1 --off
+    xrandr --output HDMI2 --mode 1920x1200 --right-of eDP-1
+    xrandr --output eDP-1 --off
 fi
 
-xrandr | grep -q "^DP1-1 connected"
+xrandr | grep -q "^DP-1-1 connected"
 if [ $? -eq 0 ]; then
     echo Found right home screen
-    xrandr --output DP1-1 --mode 1920x1200 --right-of HDMI2
+    xrandr --output DP-1-1 --mode 1920x1200 --right-of HDMI-2
 fi
