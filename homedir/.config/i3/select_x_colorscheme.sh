7c4e5f3b2ca41481063beeff2f4770ebe47fe63c
Jonas Collberg <jonas.collberg@gmail.com>
2018-06-18 00:41:52 +0200

    Add handling of spaces in palette filenames

--- homedir/.config/i3/select_x_colorscheme.sh
+++ homedir/.config/i3/select_x_colorscheme.sh
@@ -9,8 +9,11 @@ while IFS= read -d $'\0' -r f; do
     scheme_names+="$name\n"
 done < <(/usr/bin/find "$scheme_path" -maxdepth 1 -type f -print0)
 sel_scheme=$(echo -e "$scheme_names" | /usr/bin/dmenu -i -p 'Select scheme')
-echo "#define SELECTED_THEME $sel_scheme" > ~/.selected_theme.h
+echo "#define SELECTED_THEME \"$sel_scheme\"" > ~/.selected_theme.h
 if [ -e "${schemes["$sel_scheme"]}" ]; then
-    xrdb -m ${schemes["$sel_scheme"]}
+    tmp_file=$(mktemp "/tmp/palette.txt.XXX")
+    cp "${schemes["$sel_scheme"]}" $tmp_file
+    xrdb -m $tmp_file
+    rm "$tmp_file"
     urxvt
 fi
