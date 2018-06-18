b471c515e96696d12f09f5c0bd5f6b37df834a50
Jonas Collberg <jonas.collberg@gmail.com>
2018-06-18 13:05:14 +0200

    i3: Modify `goto_project.sh` to ignore hidden directories

--- homedir/.config/i3/goto_project.sh
+++ homedir/.config/i3/goto_project.sh
@@ -5,9 +5,18 @@ dirlist=()
 
 while IFS= read -d $'\0' -r file; do
     dir=$(basename "$(dirname "$file")")
+
+    hidden=0
+    IFS='/' read -ra dir_parts <<< "$file"
+    unset 'dir_parts[${#dir_parts[@]}-1]'
+    for p in "${dir_parts[@]}"; do
+        [[ "$p" =~ (^\.|___) ]] && hidden=1 && break
+    done
+    if [ "$hidden" -ne 0 ]; then continue; fi
+
     git_projects["$dir"]="$(dirname "$file")"
     dirlist+="$dir\n"
-done < <(fd -H -t d --print0 '^.git$' ~)
+done < <(fd -H -t d -0 '^.git$' ~)
 
 choice=$(echo -e "$dirlist" | dmenu -i -p "Select project")
 if [ -n "$choice" ]; then
