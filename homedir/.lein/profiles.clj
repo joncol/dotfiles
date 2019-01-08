874fe36d37f7189fea27492374d7e2d80ecbfa52
Jonas Collberg <jonas.collberg@gmail.com>
2019-01-08 12:15:22 +0100

    Update `profiles.clj`

--- homedir/.lein/profiles.clj
+++ homedir/.lein/profiles.clj
@@ -1,11 +1,10 @@
-{:user {:dependencies [[org.clojure/clojure "1.9.0"]
-                       [org.clojure/tools.trace "0.7.9"]
-                       [spyscope "0.1.7-SNAPSHOT"]]
-        :injections [(require 'spyscope.core)
-                     (use 'clojure.repl)
+{:user {:dependencies [#_[org.clojure/clojure "1.9.0"]
+                       #_[org.clojure/tools.trace "0.7.9"]
+                       #_[spyscope "0.1.7-SNAPSHOT"]]
+        :injections [#_(require 'spyscope.core)
+                     #_(use 'clojure.repl)
                      #_(use 'no.disassemble)]
-        :plugins [[cider/cider-nrepl "0.18.0"]
-                  #_[lein-nodisassemble "0.1.3"]
+        :plugins [#_[lein-nodisassemble "0.1.3"]
                   [lein-ancient "0.6.15"]
                   [lein-cljfmt "0.5.7"]
                   [lein-kibit "0.1.6"]
