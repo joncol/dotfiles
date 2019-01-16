c6dd2f615a1185d01a4f5bc15d405c08885e7a44
Jonas Collberg <jco@jco-thinkpad.mydomain>
2019-01-16 13:41:17 +0100

    Add package: `all-the-icons-dired`

--- lisp/init-common.el
+++ lisp/init-common.el
@@ -136,6 +136,10 @@
 
 (use-package ahk-mode)
 
+(use-package all-the-icons-dired
+  :config
+  (add-hook 'dired-mode-hook 'all-the-icons-dired-mode))
+
 (use-package all-the-icons-ivy
   :config
   (all-the-icons-ivy-setup))
