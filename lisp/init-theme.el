22b003cff0ac009f89715410a326d18158d69511
Jonas Collberg <jco@jco-arch.mydomain>
2019-01-27 13:33:54 +0100

    Fix company colors for text mode

--- lisp/init-theme.el
+++ lisp/init-theme.el
@@ -419,6 +419,23 @@
 
 (blink-cursor-mode -1)
 
+(unless (display-graphic-p)
+  (custom-set-faces
+   '(company-preview
+     ((t (:foreground "darkgray" :underline t))))
+   '(company-preview-common
+     ((t (:inherit company-preview))))
+   '(company-tooltip
+     ((t (:background "lightgray" :foreground "black"))))
+   '(company-tooltip-selection
+     ((t (:background "steelblue" :foreground "white"))))
+   '(company-tooltip-common
+     ((((type x)) (:inherit company-tooltip :weight bold))
+      (t (:inherit company-tooltip))))
+   '(company-tooltip-common-selection
+     ((((type x)) (:inherit company-tooltip-selection :weight bold))
+      (t (:inherit company-tooltip-selection))))))
+
 (provide 'init-theme)
 
 ;;; init-theme.el ends here
