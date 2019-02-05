cc63c1bb6b07415f690e894bd77b27852057eabd
Jonas Collberg <jco@jco-thinkpad.mydomain>
2019-02-05 11:01:50 +0100

    Add `rfcview.el`

--- lisp/init-common.el
+++ lisp/init-common.el
@@ -113,6 +113,11 @@
 
 (setq compilation-scroll-output t)
 
+(add-to-list 'auto-mode-alist
+             '("/\\(rfc\\|std\\)[0-9]+\\.txt\\'" . rfcview-mode))
+
+(autoload 'rfcview-mode "rfcview" nil t)
+
 (use-package ace-isearch
   :disabled t
   :config
