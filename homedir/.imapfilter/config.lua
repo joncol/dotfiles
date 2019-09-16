b9a7e512cedf96efdbfdfa5534aaa63e9953a404
Jonas Collberg <jonas.collberg@gmail.com>
2019-09-16 09:10:08 +0200

    Filter email from AWS

--- homedir/.imapfilter/config.lua
+++ homedir/.imapfilter/config.lua
@@ -90,3 +90,8 @@ tink_statuspage = zimpler_account.INBOX:contain_from("noreply@statuspage.io") *
                   zimpler_account.INBOX:contain_subject("Tink Enterprise Incident")
 
 tink_statuspage:mark_seen()
+
+aws = zimpler_account.INBOX:contain_from("Amazon Web Services") +
+      zimpler_account.INBOX:contain_from("no-reply-aws@amazon.com") +
+      zimpler_account.INBOX:contain_from("aws-marketing-email-replies@amazon.com")
+aws:mark_seen()
