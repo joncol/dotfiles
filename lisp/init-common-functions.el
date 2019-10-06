e462b13be2219e8062810f3f009160c5998d260a
Jonas Collberg <jco@jco-arch.mydomain>
2019-10-06 08:33:58 +0200

    Update `jco/update-config`
    Change use of obsolete `magit-git-pull` to `magit-pull-from-pushremote`.

--- lisp/init-common-functions.el
+++ lisp/init-common-functions.el
@@ -35,7 +35,7 @@
 (defun jco/update-config ()
   "Get the latest config from source control."
   (shell-process-pushd user-emacs-directory)
-  (magit-git-pull "origin/master" nil)
+  (magit-pull-from-pushremote nil)
   (shell-process-popd "1"))
 
 (defun jco/at-office-p (&optional print-message)
