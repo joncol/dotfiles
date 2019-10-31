5e2835f334c19926d0cff330665ef71ac2026d80
Jonas Collberg <jonas.collberg@gmail.com>
2019-10-31 15:57:26 +0100

    Modify `db-connect.sh` to also look for `POSTGRES_PASSWORD`

--- homedir/.local/bin/db-connect.sh
+++ homedir/.local/bin/db-connect.sh
@@ -10,7 +10,17 @@ TUNNEL_HOST="$USER@bastion.zimpler.net"
 
 DB_DATABASE=$1_$2
 DB_HOST=$1-$2-db-replica.czldyizapuwt.eu-central-1.rds.amazonaws.com
+
 DB_PASSWORD=$(ze-read $1 $2 DB_PASSWORD | awk '{print $4}')
+if [ -z "$DB_PASSWORD" ]; then
+    DB_PASSWORD=$(ze-read $1 $2 POSTGRES_PASSWORD | awk '{print $4}')
+fi
+
+if [ -z "$DB_PASSWORD" ]; then
+    echo "Couldn't find DB_PASSWORD or POSTGRES_PASSWORD in parameter store"
+    exit 1
+fi
+
 DB_PORT=5432
 DB_USER=$1_$2
 LOCAL_PORT=9004
