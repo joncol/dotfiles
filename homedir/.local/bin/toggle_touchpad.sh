16fbd6f6011cc93952b3e9fd5e16d4f35b1ee0f4
Jonas Collberg <jonas.collberg@gmail.com>
2019-12-27 13:29:18 +0100

    Add command to toggle touchpad

---
+++ homedir/.local/bin/toggle_touchpad.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+touchpad=$(xinput list | grep -i "synaptics touchpad" | cut -f2 | sed s/id=//)
+if [[ ! -z "${touchpad// }" ]]; then
+    device_enabled_pid=$(xinput list-props $touchpad | grep -i "device enabled" | sed "s/[^(]*(\(.*\)).*/\1/")
+    device_enabled=$(xinput list-props $touchpad | grep -i "device enabled" | cut -f3)
+    echo "Touchpad device ID: $touchpad"
+    if [ $device_enabled -eq 1 ]; then
+        echo "Disabling touchpad"
+        xinput set-prop $touchpad $device_enabled_pid 0
+    else
+        echo "Enabling touchpad"
+        xinput set-prop $touchpad $device_enabled_pid 1
+    fi
+fi
