08519c6d47318b86e4e97ff090484e6f793ff661
Jonas Collberg <jonas.collberg@gmail.com>
2020-06-05 17:33:53 +0200

    Use both shifts to toggle between keyboard layouts
    Doesn't cause conflicts with dwm bindings.

--- lightdm/etc/lightdm/init_xkb.sh
+++ lightdm/etc/lightdm/init_xkb.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 # Use this as display-setup-script in `lightdm.conf`.
-setxkbmap -model pc105 -layout us,us -variant colemak,altgr-intl -option -option "grp:win_space_toggle,ctrl:nocaps,terminate:ctrl_alt_bksp"
+setxkbmap -model pc105 -layout us,us -variant colemak,altgr-intl -option -option "grp:shifts_toggle,ctrl:nocaps,terminate:ctrl_alt_bksp"
