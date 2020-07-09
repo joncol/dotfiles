bc55df0082916ab36de2b551a5d9ddce7a44f9e7
Jonas Collberg <jonas.collberg@gmail.com>
2020-07-09 09:34:48 +0200

    Lower-case variable names in `db-connect.sh`

--- homedir/.local/bin/db-connect.sh
+++ homedir/.local/bin/db-connect.sh
@@ -9,33 +9,33 @@ if [[ ${PIPESTATUS[0]} -ne 4 ]]; then
     exit 1
 fi
 
-OPTIONS=v,p:,t
-LONGOPTS=verbose,port:,tunnel-only
+options=v,p:,t
+longopts=verbose,port:,tunnel-only
 
-! PARSED=$(getopt --options=$OPTIONS --longoptions=$LONGOPTS --name "$0" -- "$@")
+! parsed=$(getopt --options=$options --longoptions=$longopts --name "$0" -- "$@")
 if [[ ${PIPESTATUS[0]} -ne 0 ]]; then
     exit 2
 fi
 
-eval set -- "$PARSED"
+eval set -- "$parsed"
 
-DB_PORT=5432
-LOCAL_PORT=9004
-TUNNEL_ONLY=false
-VERBOSE=false
+db_port=5432
+local_port=9004
+tunnel_only=false
+verbose=false
 
 while true; do
     case "$1" in
         -p|--port)
-            LOCAL_PORT=$2
+            local_port=$2
             shift 2
             ;;
         -t|--tunnel-only)
-            TUNNEL_ONLY=true
+            tunnel_only=true
             shift
             ;;
         -v|--verbose)
-            VERBOSE=true
+            verbose=true
             shift
             ;;
         --)
@@ -54,64 +54,64 @@ if [[ $# -ne 2 ]]; then
     exit 4
 fi
 
-SERVICE=$1
-SERVICE_SNAKE_CASE=$(echo $SERVICE | sed 's/-/_/g')
-ENVIRONMENT=$2
-TUNNEL_HOST="bastion.zimpler.net"
+service=$1
+service_snake_case=$(echo $service | sed 's/-/_/g')
+environment=$2
+tunnel_host="bastion.zimpler.net"
 
-DB_HOST=$(ze-read $SERVICE $ENVIRONMENT POSTGRES_HOST | awk '{print $4}')
-if [ -z "$DB_HOST" ]; then
-    DB_HOST=$(ze-read $SERVICE $ENVIRONMENT DB_HOST | awk '{print $4}')
+db_host=$(ze-read $service $environment POSTGRES_HOST | awk '{print $4}')
+if [ -z "$db_host" ]; then
+    db_host=$(ze-read $service $environment db_host | awk '{print $4}')
 fi
-if [ -z "$DB_HOST" ]; then
-    DB_HOST=$SERVICE-$ENVIRONMENT-db.czldyizapuwt.eu-central-1.rds.amazonaws.com
+if [ -z "$db_host" ]; then
+    db_host=$service-$environment-db.czldyizapuwt.eu-central-1.rds.amazonaws.com
 fi
 
-DB_DATABASE=$(ze-read $SERVICE $ENVIRONMENT POSTGRES_DATABASE | \
+db_database=$(ze-read $service $environment POSTGRES_DATABASE | \
                   awk '{print $4}')
-if [ -z "$DB_DATABASE" ]; then
-    DB_DATABASE=$(printf "%s_%s" $SERVICE_SNAKE_CASE $ENVIRONMENT)
+if [ -z "$db_database" ]; then
+    db_database=$(printf "%s_%s" $service_snake_case $environment)
 fi
 
-DB_USER=$(ze-read $SERVICE $ENVIRONMENT POSTGRES_USER | awk '{print $4}')
-if [ -z "$DB_USER" ]; then
-    DB_USER=$(ze-read $SERVICE $ENVIRONMENT DB_USER | awk '{print $4}')
+db_user=$(ze-read $service $environment POSTGRES_USER | awk '{print $4}')
+if [ -z "$db_user" ]; then
+    db_user=$(ze-read $service $environment db_user | awk '{print $4}')
 fi
 
-if [ -z "$DB_USER" ]; then
-    DB_USER=${SERVICE}_$ENVIRONMENT
+if [ -z "$db_user" ]; then
+    db_user=${service}_$environment
 fi
 
-DB_PASSWORD=$(ze-read $SERVICE $ENVIRONMENT DB_PASSWORD | awk '{print $4}')
-if [ -z "$DB_PASSWORD" ]; then
-    DB_PASSWORD=$(ze-read $SERVICE $ENVIRONMENT DB_PASS | \
+db_password=$(ze-read $service $environment db_password | awk '{print $4}')
+if [ -z "$db_password" ]; then
+    db_password=$(ze-read $service $environment DB_PASS | \
                       awk '{print $4}')
 fi
-if [ -z "$DB_PASSWORD" ]; then
-    DB_PASSWORD=$(ze-read $SERVICE $ENVIRONMENT POSTGRES_PASSWORD | \
+if [ -z "$db_password" ]; then
+    db_password=$(ze-read $service $environment POSTGRES_PASSWORD | \
                       awk '{print $4}')
 fi
 
-if [ -z "$DB_PASSWORD" ]; then
-    echo "Couldn't find DB_PASSWORD or POSTGRES_PASSWORD in parameter store"
+if [ -z "$db_password" ]; then
+    echo "Couldn't find db_password or POSTGRES_PASSWORD in parameter store"
     exit 1
 fi
 
-if [ $VERBOSE = true ]; then
-    echo "Local port:    $LOCAL_PORT"
-    echo "Database host: $DB_HOST"
-    echo "Database name: $DB_DATABASE"
-    echo "Username:      $DB_DATABASE"
-    echo "Password:      $DB_PASSWORD"
+if [ $verbose = true ]; then
+    echo "Local port:    $local_port"
+    echo "Database host: $db_host"
+    echo "Database name: $db_database"
+    echo "Username:      $db_database"
+    echo "Password:      $db_password"
     echo ""
 fi
 
-if [ $TUNNEL_ONLY = true ]; then
+if [ $tunnel_only = true ]; then
     echo "Setting up tunnel to database host..."
-    ssh -NL $LOCAL_PORT:$DB_HOST:$DB_PORT $TUNNEL_HOST
+    ssh -NL $local_port:$db_host:$db_port $tunnel_host
 else
-    ssh -fL $LOCAL_PORT:$DB_HOST:$DB_PORT $TUNNEL_HOST sleep 10
-    PGHOST=localhost PGDATABASE=$DB_DATABASE PGUSER=$DB_USER \
-          PGPASSWORD=$DB_PASSWORD PGPORT=$LOCAL_PORT \
+    ssh -fL $local_port:$db_host:$db_port $tunnel_host sleep 10
+    PGHOST=localhost PGDATABASE=$db_database PGUSER=$db_user \
+          PGPASSWORD=$db_password PGPORT=$local_port \
           psql
 fi
