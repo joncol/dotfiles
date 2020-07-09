7eb355155aac29bc3ed9ea766da075371a63713f
Jonas Collberg <jonas.collberg@gmail.com>
2020-07-09 12:00:22 +0200

    Enable Wacom touchpad and make it use natural scrolling

--- homedir/.local/bin/setup-wacom.sh
+++ homedir/.local/bin/setup-wacom.sh
@@ -6,8 +6,13 @@ finger_id=$(xinput list | grep -iE 'Wacom Intuos Pro S Finger' | \
             cut -f2 | sed s/id=//)
 
 if [[ -n $finger_id ]]; then
-    echo "Disabling Wacom touch (device ID: $finger_id)"
-    xinput disable $finger_id
+    # echo "Disabling Wacom touch (device ID: $finger_id)"
+    # xinput disable $finger_id
+
+    wacom_nat_scroll=$(xinput list-props $finger_id | grep -i "natural scrolling enabled (" | sed "s/[^(]*(\(.*\)).*/\1/")
+    echo "Wacom touchpad device ID: $finger_id"
+    echo "  Natural scrolling property ID: $wacom_nat_scroll"
+    xinput set-prop $finger_id $wacom_nat_scroll 1
 fi
 
 pen_id=$(xinput list | grep -iE 'Wacom Intuos Pro S Pen Pen' | \
