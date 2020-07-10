6f92d578e9aaad9fdee6dbb1dd717ec9cd7574c8
Jonas Collberg <jonas.collberg@gmail.com>
2020-07-10 10:23:24 +0200

    alacritty: Add script to select color theme

---
+++ homedir/.local/bin/select-alacritty-theme.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+declare -A themes
+theme_path=~/.config/alacritty/alacritty-theme/themes
+theme_names=()
+while IFS= read -d $'\0' -r f; do
+    name=$(basename ${f%.*})
+    themes["$name"]="$f"
+    theme_names+="$name\n"
+done < <(/usr/bin/find "$theme_path" -maxdepth 1 -type f -print0)
+sel_theme=$(echo -e "$theme_names" | \
+                dmenu -fn 'Montserrat-12:medium:antialias=true' \
+                      -h 20 -dim 0.4 -y 2 -sf "#3f3f3f" -sb "#4bcffa" \
+                      -i -p 'Select theme')
+sel_theme_file="${themes["$sel_theme"]}"
+if [ -e "$sel_theme_file" ]; then
+    ~/.local/bin/alacritty-colorscheme \
+        -C ~/.config/alacritty/alacritty-theme/themes \
+        -a $(basename $sel_theme_file)
+    notify-send "Theme selected: $sel_theme"
+fi
