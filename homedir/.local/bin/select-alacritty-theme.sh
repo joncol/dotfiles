1ad698bd4d375ccf63f003f6a1ecaed0735e7d8d
Jonas Collberg <jonas.collberg@gmail.com>
2020-07-12 09:52:12 +0200

    alacritty: Sort themes when selecting

--- homedir/.local/bin/select-alacritty-theme.sh
+++ homedir/.local/bin/select-alacritty-theme.sh
@@ -7,7 +7,7 @@ while IFS= read -d $'\0' -r f; do
     name=$(basename ${f%.*})
     themes["$name"]="$f"
     theme_names+="$name\n"
-done < <(/usr/bin/find "$theme_path" -maxdepth 1 -type f -print0)
+done < <(/usr/bin/find "$theme_path" -maxdepth 1 -type f -print0 | sort -z)
 sel_theme=$(echo -e "$theme_names" | \
                 dmenu -fn 'Montserrat-12:medium:antialias=true' \
                       -h 20 -dim 0.4 -y 2 -sf "#3f3f3f" -sb "#4bcffa" \
