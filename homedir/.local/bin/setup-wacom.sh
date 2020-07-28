259302f0fe252bde31bb9782369e0feeb46323e8
Jonas Collberg <jonas.collberg@gmail.com>
2020-07-28 14:12:21 +0200

    Make `setup-wacom.sh` check for existence of `xsetwacom` command

--- homedir/.local/bin/setup-wacom.sh
+++ homedir/.local/bin/setup-wacom.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
-xsetwacom --set "Wacom Intuos Pro S Pen stylus" area -31920 0 31920 19950
-xsetwacom --set "Wacom Intuos Pro S Pen eraser" area -31920 0 31920 19950
+if command -v xsetwacom &> /dev/null; then
+    xsetwacom --set "Wacom Intuos Pro S Pen stylus" area -31920 0 31920 19950
+    xsetwacom --set "Wacom Intuos Pro S Pen eraser" area -31920 0 31920 19950
+fi
