5df52f90e6c78f96e7ac1ee29e8afbde192c9f68
Jonas Collberg <jonas.collberg@gmail.com>
2021-01-20 08:32:06 +0100

    emacs: Add `org-roam-create-note-from-headline` function

--- homedir/.config/emacs/lisp/init-org.el
+++ homedir/.config/emacs/lisp/init-org.el
@@ -240,6 +240,34 @@ PRIORITY may be one of the characters ?A, ?B, or ?C."
     '(require 'ox-gfm nil t))
   (add-hook 'org-capture-mode-hook 'evil-insert-state))
 
+;; Source: https://org-roam.discourse.group/t/creating-an-org-roam-note-from-an-existing-headline/978
+(defun org-roam-create-note-from-headline ()
+  "Create an Org-roam note from the current headline and jump to it.
+
+Normally, insert the headline’s title using the ’#title:’ file-level property
+and delete the Org-mode headline. However, if the current headline has a
+Org-mode properties drawer already, keep the headline and don’t insert
+‘#+title:'. Org-roam can extract the title from both kinds of notes, but using
+‘#+title:’ is a bit cleaner for a short note, which Org-roam encourages."
+  (interactive)
+  (org-cycle 2)
+  (let ((title (nth 4 (org-heading-components)))
+        (has-properties (org-get-property-block)))
+    (org-cut-subtree)
+    (org-roam-find-file title nil nil 'no-confirm)
+    (org-paste-subtree)
+    (unless has-properties
+      (kill-line)
+      (while (outline-next-heading)
+        (org-promote)))
+    (goto-char (point-min))
+    (when has-properties
+      (kill-line)
+      (kill-line))
+    (goto-char (point-at-pos-rel-line-offset (point-min) 3))
+    (delete-blank-lines)
+    (indent-region (point-min) (point-max))))
+
 (use-package org-roam
   :hook
   (after-init . org-roam-mode)
