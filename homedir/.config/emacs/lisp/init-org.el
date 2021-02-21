3f6d699e5426aeac62d19fb54a194c82ba2b9abe
Jonas Collberg <jonas.collberg@gmail.com>
2021-02-21 11:02:09 +0100

    emacs: Add sound effects for `org-pomodoro`
    Sources:
    http://onj3.andrelouis.com/phonetones/unzipped/Meizu%20MX4/notifications/Ripples.mp3
    http://tastyspleen.net/~quake2/baseq2/sound/world/clock.wav

--- homedir/.config/emacs/lisp/init-org.el
+++ homedir/.config/emacs/lisp/init-org.el
@@ -434,16 +434,21 @@ As such, it will only work when the notes window exists."
 (use-package org-pomodoro
   :custom
   (org-pomodoro-manual-break t)
+
+  (org-pomodoro-start-sound-p t)
   (org-pomodoro-start-sound (concat user-emacs-directory
-                                    "resources/sounds/ripple.wav"))
+                                    "resources/sounds/ripples.wav"))
   (org-pomodoro-finished-sound (concat user-emacs-directory
-                                       "resources/sounds/ripple.wav"))
+                                       "resources/sounds/ripples.wav"))
   (org-pomodoro-overtime-sound (concat user-emacs-directory
-                                       "resources/sounds/ripple.wav"))
+                                       "resources/sounds/ripples.wav"))
   (org-pomodoro-short-break-sound (concat user-emacs-directory
-                                          "resources/sounds/ripple.wav"))
+                                          "resources/sounds/ripples.wav"))
   (org-pomodoro-long-break-sound (concat user-emacs-directory
-                                         "resources/sounds/ripple.wav")))
+                                         "resources/sounds/ripples.wav"))
+  (org-pomodoro-killed-sound-p t)
+  (org-pomodoro-killed-sound (concat user-emacs-directory
+                                     "resources/sounds/clock.wav")))
 
 (use-package org-ref
   :after org
