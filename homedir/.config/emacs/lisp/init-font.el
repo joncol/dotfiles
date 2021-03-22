04cf4c4ccaa672ffcfb6f4c77ad12667b2e260e8
Jonas Collberg <jonas.collberg@gmail.com>
2021-03-22 15:21:35 +0100

    emacs: Add `ligature-mode`

--- homedir/.config/emacs/lisp/init-font.el
+++ homedir/.config/emacs/lisp/init-font.el
@@ -29,6 +29,34 @@
       (set-frame-size (selected-frame) 93 60))
     (set-face-attribute 'default nil :height 145))))
 
+(use-package ligature
+  :straight (:host github :repo "mickeynp/ligature.el")
+  :config
+  ;; Enable the "www" ligature in every possible major mode
+  (ligature-set-ligatures 't '("www"))
+  ;; Enable traditional ligature support in eww-mode, if the
+  ;; `variable-pitch' face supports it
+  (ligature-set-ligatures 'eww-mode '("ff" "fi" "ffi"))
+  ;; Enable all Cascadia Code ligatures in programming modes
+  (ligature-set-ligatures
+   'prog-mode '("-->" "//" "/**" "/*" "*/" "<!--" ":=" "->>" "<<-" "->" "<-"
+                "<=>" "==" "!=" "<=" ">=" "=:=" "!==" "&&" "||" "..." ".."
+                "|||" "///" "&&&" "===" "++" "--" "=>" "|>" "<|" "||>" "<||"
+                "|||>" "<|||" ">>" "<<" "::=" "|]" "[|" "{|" "|}"
+                "[<" ">]" ":?>" ":?" "/=" "[||]" "!!" "?:" "?." "::"
+                "+++" "??" "###" "##" ":::" "####" ".?" "?=" "=!=" "<|>"
+                "<:" ":<" ":>" ">:" "<>" "***" ";;" "/==" ".=" ".-" "__"
+                "=/=" "<-<" "<<<" ">>>" "<=<" "<<=" "<==" "<==>" "==>" "=>>"
+                ">=>" ">>=" ">>-" ">-" "<~>" "-<" "-<<" "=<<" "---" "<-|"
+                "<=|" "/\\" "\\/" "|=>" "|~>" "<~~" "<~" "~~" "~~>" "~>"
+                "<$>" "<$" "$>" "<+>" "<+" "+>" "<*>" "<*" "*>" "</>" "</" "/>"
+                "<->" "..<" "~=" "~-" "-~" "~@" "^=" "-|" "_|_" "|-" "||-"
+                "|=" "||=" "#{" "#[" "]#" "#(" "#?" "#_" "#_(" "#:" "#!" "#="
+                "&="))
+  ;; Enables ligature checks globally in all buffers. You can also do it
+  ;; per mode with `ligature-mode'.
+  (global-ligature-mode t))
+
 (provide 'init-font)
 
 ;;; init-font.el ends here
