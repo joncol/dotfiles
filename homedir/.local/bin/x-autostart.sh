23362b71ab83b5e848a076aacffbc632bfc914ac
Jonas Collberg <jonas.collberg@gmail.com>
2021-05-09 10:43:54 +0200

    Consolidate keyboard settings
    Move `xset` command to `x-init-kb.sh`.

--- homedir/.local/bin/x-autostart.sh
+++ homedir/.local/bin/x-autostart.sh
@@ -8,8 +8,6 @@
 # picom --unredir-if-possible --experimental-backends -b
 ~/.local/bin/launch.sh redshift &
 
-xset r rate 300 30
-
 # ~/.local/bin/launch.sh xcape -t 150 -e 'Control_L=BackSpace'
 
 currenttime=$(date +%H:%M)
