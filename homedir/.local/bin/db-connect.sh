ebd7ed8d9b5e85444106845944110671e518cf69
Jonas Collberg <jonas.collberg@gmail.com>
2021-05-27 08:46:26 +0200

    db-connect.sh: Update to make compatible with Hippo

--- homedir/.local/bin/db-connect.sh
+++ homedir/.local/bin/db-connect.sh
@@ -60,17 +60,23 @@ environment=$2
 
 db_host=$(ze-read -v $service $environment shared POSTGRES_HOST)
 db_host=${db_host:-$(ze-read -v $service $environment shared DB_HOST)}
+db_host=${db_host:-$(ze-read -v $service $environment shared DATABASE_HOST)}
 db_host=${db_host:-$service-$environment.czldyizapuwt.eu-central-1.rds.amazonaws.com}
 
 db_database=$(ze-read -v $service $environment shared POSTGRES_DATABASE)
+db_database=${db_database:-$(ze-read -v $service $environment shared \
+    DATABASE_NAME)}
 db_database=${db_database:-"${service_snake_case}_$environment"}
 
 db_user=$(ze-read -v $service $environment shared POSTGRES_USER)
 db_user=${db_user:-$(ze-read -v $service $environment shared DB_USER)}
+db_user=${db_user:-$(ze-read -v $service $environment shared DATABASE_USER)}
 db_user=${db_user:-${service}_$environment}
 
 db_password=$(ze-read -v $service $environment shared DB_PASSWORD)
 db_password=${db_password:-$(ze-read -v $service $environment shared DB_PASS)}
+db_password=${db_password:-$(ze-read -v $service $environment shared \
+    DATABASE_PASS)}
 db_password=${db_password:-$(ze-read -v $service $environment shared \
     POSTGRES_PASSWORD)}
 
