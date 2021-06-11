486d5df481bb6e2624ce1d86141e88fc4855b7fd
Jonas Collberg <jonas.collberg@gmail.com>
2021-06-11 13:32:43 +0200

    db-connect.sh: Use snake_case for username

--- homedir/.local/bin/db-connect.sh
+++ homedir/.local/bin/db-connect.sh
@@ -71,7 +71,7 @@ db_database=${db_database:-"${service_snake_case}_$environment"}
 db_user=$(ze-read -v $service $environment shared POSTGRES_USER)
 db_user=${db_user:-$(ze-read -v $service $environment shared DB_USER)}
 db_user=${db_user:-$(ze-read -v $service $environment shared DATABASE_USER)}
-db_user=${db_user:-${service}_$environment}
+db_user=${db_user:-${service_snake_case}_$environment}
 
 db_password=$(ze-read -v $service $environment shared DB_PASSWORD)
 db_password=${db_password:-$(ze-read -v $service $environment shared DB_PASS)}
