2129715ec991610caaf8116fd3c311f32b93e5b9
Jonas Collberg <jonas.collberg@gmail.com>
2021-07-19 11:04:12 +0200

    emacs: Add package: `embark`

--- homedir/.config/emacs/init.el
+++ homedir/.config/emacs/init.el
@@ -4421,6 +4421,33 @@ accordance with ISO 8601)."
   :init
   (marginalia-mode))
 
+(use-package embark
+  :bind
+  (("C-." . embark-act)         ;; pick some comfortable binding
+   ("C-;" . embark-dwim)        ;; good alternative: M-.
+   ("C-h B" . embark-bindings)) ;; alternative for `describe-bindings'
+
+  :init
+
+  ;; Optionally replace the key help with a completing-read interface.
+  (setq prefix-help-command #'embark-prefix-help-command)
+
+  :config
+
+  ;; Hide the mode line of the Embark live/completions buffers.
+  (add-to-list 'display-buffer-alist
+               '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
+                 nil
+                 (window-parameters (mode-line-format . none)))))
+
+(use-package embark-consult
+  :after (embark consult)
+  :demand t ; only necessary if you have the hook below
+  ;; if you want to have consult previews as you move around an
+  ;; auto-updating embark collect buffer
+  :hook
+  (embark-collect-mode . consult-preview-at-point-mode))
+
 (use-package yasnippet
   :defer t
   :config
