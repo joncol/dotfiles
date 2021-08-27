8b8d1ac6f5eb7081eb279ccd397088e3fc6af6ca
Jonas Collberg <jonas.collberg@gmail.com>
2021-08-27 11:32:53 +0200

    Make `toggle-touchpad.sh` work with Dell laptop

--- homedir/.local/bin/toggle-touchpad.sh
+++ homedir/.local/bin/toggle-touchpad.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-touchpad=$(xinput list | grep -i "synaptics" | cut -f2 | sed s/id=//)
+touchpad=$(xinput list | grep -iE 'synapics|touchpad' | cut -f2 | sed s/id=//)
 if [[ ! -z "${touchpad// }" ]]; then
     device_enabled_pid=$(xinput list-props $touchpad | grep -i "device enabled" | sed "s/[^(]*(\(.*\)).*/\1/")
     device_enabled=$(xinput list-props $touchpad | grep -i "device enabled" | cut -f3)
@@ -11,4 +11,6 @@ if [[ ! -z "${touchpad// }" ]]; then
         echo "Enabling touchpad"
         xinput set-prop $touchpad $device_enabled_pid 1
     fi
+else
+    echo "No touchpad device found"
 fi
