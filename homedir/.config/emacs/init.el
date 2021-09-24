ff7671710da511ae4c128cc102d825f930a6fd7b
Jonas Collberg <jonas.collberg@gmail.com>
2021-09-24 07:40:16 +0200

    emacs: Do not treat "-" as a word separator in `helpful-mode`

--- homedir/.config/emacs/init.el
+++ homedir/.config/emacs/init.el
@@ -4170,7 +4170,12 @@ repo."
   :init
   (global-set-key (kbd "C-h f") #'helpful-callable)
   (global-set-key (kbd "C-h v") #'helpful-variable)
-  (global-set-key (kbd "C-h k") #'helpful-key))
+  (global-set-key (kbd "C-h k") #'helpful-key)
+  :config
+  (add-hook 'helpful-mode-hook
+            (lambda ()
+              ;; do not treat "-" as a word separator
+              (modify-syntax-entry ?- "w"))))
 
 (use-package olivetti
   :defer
