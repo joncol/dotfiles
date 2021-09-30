94416a8917a87ca1f273e4f3a8a4d2b948e074b8
Jonas Collberg <jonas.collberg@gmail.com>
2021-09-30 19:32:45 +0200

    emacs: Use `org-mode` variable to set current theme
    Putting this at the top to facilitate easier changing of themes.

--- homedir/.config/emacs/init.el
+++ homedir/.config/emacs/init.el
@@ -1,3 +1,8 @@
+(let ((my-theme '"doom-one"))
+(defvar jco/theme)
+(setq jco/theme (intern my-theme))
+)
+
 (defvar bootstrap-version)
 (let ((bootstrap-file
                (expand-file-name "straight/repos/straight.el/bootstrap.el"
@@ -3580,69 +3585,6 @@ repo."
   :config
   (setq undo-tree-visualizer-diff t)
   (setq undo-tree-visualizer-timestamps t))
-
-(defvar jco/theme)
-
-;; My current theme. Change this to whatever theme you want.
-(setq jco/theme
-      ;; 'adwaita
-      ;; 'borland-blue
-      ;; 'chyla
-      ;; 'cyberpunk
-      ;; 'darktooth
-      ;; 'doom-acario-light
-      ;; 'doom-challenger-deep
-      ;; 'doom-city-lights
-      ;; 'doom-dark+
-      'doom-dracula
-      ;; 'doom-flatwhite
-      ;; 'doom-gruvbox
-      ;; 'doom-gruvbox-light
-      ;; 'doom-laserwave
-      ;; 'doom-manegarm
-      ;; 'doom-miramare
-      ;; 'doom-molokai
-      ;; 'doom-moonlight
-      ;; 'doom-nord-light
-      ;; 'doom-oceanic-next
-      ;; 'doom-old-hope
-      ;; 'doom-one
-      ;; 'doom-one-light
-      ;; 'doom-opera-light
-      ;; 'doom-outrun-electric
-      ;; 'doom-solarized-light
-      ;; 'doom-vibrant
-      ;; 'doom-rouge
-      ;; 'doom-snazzy
-      ;; 'doom-sourcerer
-      ;; 'doom-spacegrey
-      ;; 'doom-tomorrow-day
-      ;; 'eink
-      ;; 'gotham
-      ;; 'github-modern
-      ;; 'gruvbox-dark-hard
-      ;; 'hemisu-dark
-      ;; 'hemisu-light
-      ;; 'kaolin-bubblegum
-      ;; 'kaolin-dark
-      ;; 'kaolin-eclipse
-      ;; 'kaolin-galaxy
-      ;; 'kaolin-ocean
-      ;; 'kaolin-valley-dark
-      ;; 'minimal
-      ;; 'minimal-light
-      ;; 'mustang
-      ;; 'nubox-dark
-      ;; 'nubox-light
-      ;; 'organic-green
-      ;; 'reykjavik
-      ;; 'sanityinc-tomorrow-blue
-      ;; 'sanityinc-tomorrow-eighties
-      ;; 'sanityinc-tomorrow-night
-      ;; 'solarized-dark
-      ;; 'tao-yang
-      ;; 'tao-yin
-      )
 
 (require 'kurecolor)
 
