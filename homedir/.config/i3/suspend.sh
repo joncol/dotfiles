71003a6a1e4509e558c1de756e674854abfb9b63
Jonas Collberg <jonas.collberg@gmail.com>
2021-11-09 18:11:58 +0100

    Delete obsolete files

--- homedir/.config/i3/suspend.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-
-~/.local/bin/betterlockscreen --lock dim &
-sleep 0.5 && systemctl suspend
--- homedir/.local/bin/get-wood-artifacts.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-for REMOTE in $(aws s3 ls s3://ebway/artifacts/production/$(date +"%Y-%m-%d") | grep "$1" | rev | cut -d " " -f 1 | rev)
-do
-  aws s3 cp "s3://ebway/artifacts/production/$REMOTE" "artifact/production/$REMOTE"
-done
