7dc1997bf88b04c669b15c96ac3f6da0ea959341
Jonas Collberg <jonas.collberg@gmail.com>
2021-12-07 12:48:40 +0100

    kontrakcja: Migrate from `projectile` to `project.el`

--- work/scrive/kontrakcja/.dir-locals.el
+++ work/scrive/kontrakcja/.dir-locals.el
@@ -1,4 +1,7 @@
-((nil . ((lsp-file-watch-threshold . 20000)))
+((nil . ((project-vc-ignores . ("scrivepdftools/"
+                                "texts/"
+                                "vagrant/"))
+         (lsp-file-watch-threshold . 20000)))
  (haskell-mode . ((haskell-stylish-on-save . t)
                   (haskell-mode-stylish-haskell-path . "brittany")
                   (fill-column . 90))))
