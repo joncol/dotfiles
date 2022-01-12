b97a4cb22bba3e4c504e0a7115e97b1b8a8e9628
Jonas Collberg <jonas.collberg@gmail.com>
2022-01-12 15:53:15 +0100

    emacs: Update `custom.el`

--- homedir/.emacs.d/custom.el
+++ homedir/.emacs.d/custom.el
@@ -4,7 +4,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(safe-local-variable-values
-   '((haskell-mode-stylish-haskell-path . "brittany")
+   '((haskell-stylish-on-save)
+     (haskell-mode-stylish-haskell-path . "brittany")
      (haskell-stylish-on-save . t)
      (eval turn-off-auto-fill)
      (eval visual-fill-column-mode t)
@@ -23,4 +24,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Fira Code" :foundry "CTDB" :slant normal :weight medium :height 110 :width normal))))
  '(italic ((t (:underline t)))))
