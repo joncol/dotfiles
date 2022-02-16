261c8f72877fbaf7294faa44fe49b7da6eeecb51
Jonas Collberg <jonas.collberg@gmail.com>
2022-02-16 13:10:43 +0100

    kontrakcja/frontend: Set JavaScript indentation to two spaces

--- work/scrive/kontrakcja/frontend/.dir-locals.el
+++ work/scrive/kontrakcja/frontend/.dir-locals.el
@@ -1,3 +1,3 @@
 ((js-mode . ((fill-column . 90)
-             (evil-shift-width . 4)
-             (js-indent-level . 4))))
+             (evil-shift-width . 2)
+             (js-indent-level . 2))))
