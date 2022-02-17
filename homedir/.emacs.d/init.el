223b70f0713c58fa4bde7ce049cc3befeb9e0491
Jonas Collberg <jonas.collberg@gmail.com>
2022-02-17 12:03:48 +0100

    emacs: Add more JavaScript configuration
    Make Emacs find locally installed `eslint`, and add package `web-mode`.
    
    See: http://codewinds.com/blog/2015-04-02-emacs-flycheck-eslint-jsx.html

--- homedir/.emacs.d/init.el
+++ homedir/.emacs.d/init.el
@@ -3127,6 +3127,20 @@ Lisp function does not specify a special indentation."
                   (gradle-run "test --info")))
               (evil-leader/set-key "t s" 'gradle-single-test))))
 
+;; Use local eslint from node_modules before global one.
+;; See: http://emacs.stackexchange.com/questions/21205/flycheck-with-file-relative-eslint-executable
+(defun my/use-eslint-from-node-modules ()
+  (let* ((root (locate-dominating-file
+                (or (buffer-file-name) default-directory)
+                "node_modules"))
+         (eslint (and root
+                      (expand-file-name "node_modules/eslint/bin/eslint.js"
+                                        root))))
+    (when (and eslint (file-executable-p eslint))
+      (setq-local flycheck-javascript-eslint-executable eslint))))
+
+(add-hook 'flycheck-mode-hook #'my/use-eslint-from-node-modules)
+
 (add-hook 'js-mode-hook
           (lambda ()
             (lsp-deferred)
@@ -3142,6 +3156,31 @@ Lisp function does not specify a special indentation."
 
 (use-package js2-mode
   :mode "\\.js\\'")
+
+(add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
+
+(use-package web-mode
+  :defer
+  :config
+  (add-hook 'web-mode-hook
+            (lambda ()
+              (lsp-deferred)
+              (flycheck-add-mode 'javascript-eslint 'web-mode)
+
+              ;; This is to make the call to `flycheck-add-next-checker' work.
+              (lsp-diagnostics-mode)
+
+              (flycheck-add-next-checker 'lsp 'javascript-eslint)
+              (setq web-mode-markup-indent-offset 2)
+              (setq web-mode-css-indent-offset 2)
+              (setq web-mode-code-indent-offset 2)))
+
+  ;; For better jsx syntax-highlighting in web-mode.
+  (defadvice web-mode-highlight-part (around tweak-jsx activate)
+    (if (equal web-mode-content-type "jsx")
+        (let ((web-mode-enable-part-face nil))
+          ad-do-it)
+      ad-do-it)))
 
 (use-package jinja2-mode
   :defer)
