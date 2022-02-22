8cf6c4fce6c023d868cdf8aaafc809a127898cdd
Jonas Collberg <jonas.collberg@gmail.com>
2022-02-22 08:16:06 +0100

    scrive/kontrakcja: Disable `apheleia-mode` in `json-mode`

--- work/scrive/kontrakcja/.dir-locals.el
+++ work/scrive/kontrakcja/.dir-locals.el
@@ -2,5 +2,8 @@
                                 "texts/"
                                 "vagrant/"))
          (lsp-file-watch-threshold . 20000)))
+
  (haskell-mode . ((apheleia-formatter . brittany)
-                  (fill-column . 90))))
+                  (fill-column . 90)))
+
+ (json-mode . ((apheleia-mode -1))))
