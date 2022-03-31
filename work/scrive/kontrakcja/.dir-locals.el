e4c1120a8c4077bda864c9805110e17e6d6fc97b
Jonas Collberg <jonas.collberg@gmail.com>
2022-03-31 17:28:54 +0200

    scrive/kontrakcja: Fix to make `.conf.template` files use JSON LSP

--- work/scrive/kontrakcja/.dir-locals.el
+++ work/scrive/kontrakcja/.dir-locals.el
@@ -1,9 +1,11 @@
 ((nil . ((eval . (add-to-list 'lsp-language-id-configuration
-                              '(".*\\.conf$" . "json")))
+                              '(conf-colon-mode . "json")))
          (project-vc-ignores . ("scrivepdftools/"
                                 "texts/"
                                 "vagrant/"))
-         (lsp-file-watch-threshold . 20000)))
+         (lsp-file-watch-threshold . 20000)
+         (add-to-list 'auto-mode-alist
+                      '("\\.conf.template\\'" . conf-colon-mode))))
 
  (conf-colon-mode . ((eval . (lsp-deferred))))
 
