0842988b52e110cc437dda8af7d33638b9ac9953
Jonas Collberg <jonas.collberg@gmail.com>
2022-08-31 10:32:46 +0200

    emacs: Change theme to `ef-summer`

--- homedir/.emacs.d/init.el
+++ homedir/.emacs.d/init.el
@@ -2,7 +2,7 @@
 (when (file-exists-p custom-file)
   (load custom-file))
 
-(let ((my-theme '"modus-vivendi"))
+(let ((my-theme '"ef-summer"))
 (defvar jco/theme)
 (setq jco/theme (intern my-theme))
 )
