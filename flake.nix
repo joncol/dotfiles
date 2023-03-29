d2676893ae144d5b31b26b38417bf8cf5dc837af
Jonas Collberg <jonas.collberg@gmail.com>
2023-03-29 16:24:55 +0200

    Add basic setup for writing Neovim configuration in Lua

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "My personal dotfiles";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in rec {
+        devShells.default = pkgs.mkShell rec {
+          packages = with pkgs; [ sumneko-lua-language-server ];
+        };
+      });
+}
