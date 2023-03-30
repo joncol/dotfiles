44744afbb404f561bce0173aac73780842d50155
Jonas Collberg <jonas.collberg@gmail.com>
2023-03-30 09:59:22 +0200

    nvim: Tweak `vim_better_whitespace` config

--- homedir/.config/nvim/lua/plugins/vim_better_whitespace.lua
+++ homedir/.config/nvim/lua/plugins/vim_better_whitespace.lua
@@ -4,6 +4,6 @@ return {
     vim.g.better_whitespace_enabled = 1
     vim.g.strip_whitespace_on_save = 1
     vim.g.strip_whitespace_confirm = 0
-    vim.g.strip_only_modified_lines = 1
+    -- vim.g.strip_only_modified_lines = 1
   end
 }
