a82a04998b5e00dae44a53fa0844f0eb68709c98
Jonas Collberg <jonas.collberg@gmail.com>
2023-03-30 12:43:25 +0200

    nvim: Modify LSP config
    Make autoformatting work, and add bindings.

--- homedir/.config/nvim/lua/plugins/nvim_lspconfig.lua
+++ homedir/.config/nvim/lua/plugins/nvim_lspconfig.lua
@@ -1,3 +1,87 @@
 return {
-  "neovim/nvim-lspconfig"
+  "neovim/nvim-lspconfig",
+  config = function()
+    local lspconfig = require('lspconfig')
+    lspconfig.lua_ls.setup {}
+    lspconfig.hls.setup {}
+
+    local format_is_enabled = true
+    vim.api.nvim_create_user_command("AutoFormatToggle", function()
+      format_is_enabled = not format_is_enabled
+      print("Setting autoformatting to: " .. tostring(format_is_enabled))
+    end, {})
+
+    -- Create an augroup that is used for managing our formatting autocmds.
+    -- We need one augroup per client to make sure that multiple clients
+    -- can attach to the same buffer without interfering with each other.
+    local _augroups = {}
+    local get_augroup = function(client)
+      if not _augroups[client.id] then
+        local group_name = "my-lsp-format-" .. client.name
+        local id = vim.api.nvim_create_augroup(group_name, { clear = true })
+        _augroups[client.id] = id
+      end
+
+      return _augroups[client.id]
+    end
+
+    vim.api.nvim_create_autocmd("LspAttach", {
+      group = vim.api.nvim_create_augroup("my-lsp-attach-format", { clear = true }),
+      callback = function(args)
+        local client_id = args.data.client_id
+        local client = vim.lsp.get_client_by_id(client_id)
+        local bufnr = args.buf
+
+        -- Enable completion triggered by <c-x><c-o>
+        vim.bo[args.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+        local opts = { buffer = args.buf }
+        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+        vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+        vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts)
+        vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
+        vim.keymap.set('n', '<leader>wl', function()
+          print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+        end, opts)
+        vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
+        vim.keymap.set('n', '<leader>lr', vim.lsp.buf.rename, opts)
+        vim.keymap.set('n', '<leader>la', vim.lsp.buf.code_action, opts)
+        vim.keymap.set('n', '<leader>lgr', vim.lsp.buf.references, opts)
+        vim.keymap.set('n', '<leader>l==', function()
+          vim.lsp.buf.format { async = true }
+        end, opts)
+
+        -- Only attach to clients that support document formatting
+        if not client.server_capabilities.documentFormattingProvider then
+          return
+        end
+
+        if client.name == "tsserver" then
+          return
+        end
+
+        -- Create an autocmd that will run *before* we save the buffer.
+        -- Run the formatting command for the LSP that has just attached.
+        vim.api.nvim_create_autocmd("BufWritePre", {
+          group = get_augroup(client),
+          buffer = bufnr,
+          callback = function()
+            if not format_is_enabled then
+              return
+            end
+
+            vim.lsp.buf.format {
+              async = false,
+              filter = function(c)
+                return c.id == client.id
+              end,
+            }
+          end,
+        })
+      end,
+    })
+  end
 }
