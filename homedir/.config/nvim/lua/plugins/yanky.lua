d2412ad98ed5276cbccb6377df06df6f32a2bb25
Jonas Collberg <jonas.collberg@gmail.com>
2023-03-30 19:59:35 +0200

    nvim: Add `yanky`

---
+++ homedir/.config/nvim/lua/plugins/yanky.lua
@@ -0,0 +1,12 @@
+return {
+  "gbprod/yanky.nvim",
+  config = function()
+    vim.keymap.set({ "n", "x" }, "p", "<Plug>(YankyPutAfter)")
+    vim.keymap.set({ "n", "x" }, "P", "<Plug>(YankyPutBefore)")
+    vim.keymap.set({ "n", "x" }, "gp", "<Plug>(YankyGPutAfter)")
+    vim.keymap.set({ "n", "x" }, "gP", "<Plug>(YankyGPutBefore)")
+    vim.keymap.set("n", "<c-n>", "<Plug>(YankyCycleBackward)")
+    vim.keymap.set("n", "<c-p>", "<Plug>(YankyCycleForward)")
+    require("yanky").setup()
+  end
+}
