e87bcc20cb57f3b78975f7bd27fd83ed0c874c4c
Jonas Collberg <jonas.collberg@gmail.com>
2023-03-30 17:13:56 +0200

    nvim: Modify `indent-blankline` configuration

--- homedir/.config/nvim/lua/plugins/indent_blankline.lua
+++ homedir/.config/nvim/lua/plugins/indent_blankline.lua
@@ -1,3 +1,16 @@
 return {
-  "lukas-reineke/indent-blankline.nvim"
+  "lukas-reineke/indent-blankline.nvim",
+  event = "VeryLazy",
+  config = function()
+    vim.opt.list = true
+    -- vim.opt.listchars:append "space:⋅"
+    -- vim.opt.listchars:append "eol:↴"
+
+    require("indent_blankline").setup({
+      show_end_of_line = true,
+      space_char_blankline = " ",
+      show_current_context = true,
+      -- show_current_context_start = true,
+    })
+  end
 }
