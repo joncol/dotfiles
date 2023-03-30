5033a3d50cd2d9d068dd08a6ffce6201a3b91498
Jonas Collberg <jonas.collberg@gmail.com>
2023-03-30 09:18:36 +0200

    nvim: Fix `telescope-fzf-native` setup
    I also had to go into
    `/home/jco/.local/share/nvim/lazy/telescope-fzf-native.nvim` and run
    `make` for it to start working.

--- homedir/.config/nvim/lua/plugins/telescope.lua
+++ homedir/.config/nvim/lua/plugins/telescope.lua
@@ -1,30 +1,37 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  lazy = false,
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-telescope/telescope-fzf-native.nvim",
-    build = "make",
+  {
+    "nvim-telescope/telescope.nvim",
+    lazy = false,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    keys = {
+      {
+        "<leader>fi",
+        function()
+          require("telescope.builtin").find_files({ cwd = "~/.config/nvim", follow = true })
+        end,
+        desc = "Find init file",
+      }
+    },
     config = function()
-      require("telescope").load_extension("fzf")
-    end,
-  },
-  keys = {
-    {
-      "<leader>fi",
-      function()
-        require("telescope.builtin").find_files({ cwd = "~/.config/nvim", follow = true })
-      end,
-      desc = "Find init file",
-    }
-  },
-  config = function()
-    require("telescope").setup({})
+      require("telescope").setup({})
 
-    local builtin = require('telescope.builtin')
-    vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-    vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-    vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-    vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-  end
+      local builtin = require('telescope.builtin')
+      vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+      vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+      vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+      vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+    end
+  },
+  {
+    "telescope.nvim",
+    dependencies = {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
+      config = function()
+        require("telescope").load_extension("fzf")
+      end,
+    },
+  },
 }
