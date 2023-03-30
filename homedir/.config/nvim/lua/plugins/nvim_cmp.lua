1edf34778857745361acb1943ba461c77a8f2ae4
Jonas Collberg <jonas.collberg@gmail.com>
2023-03-30 10:01:22 +0200

    nvim: Add basic configuration of `nvim-cmp`

---
+++ homedir/.config/nvim/lua/plugins/nvim_cmp.lua
@@ -0,0 +1,56 @@
+return {
+  "hrsh7th/nvim-cmp",
+  event = "InsertEnter",
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-cmdline",
+    "hrsh7th/cmp-emoji",
+  },
+  ---@param opts cmp.ConfigSchema
+  opts = function(_, opts)
+    print("HERE AT LEAST")
+    local has_words_before = function()
+      unpack = unpack or table.unpack
+      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+      return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+    end
+
+    local luasnip = require("luasnip")
+    local cmp = require("cmp")
+
+    opts.sources = cmp.config.sources(vim.list_extend(opts.sources or {}, {
+      { name = "buffer" },
+      { name = "cmdline" },
+      { name = "emoji" },
+      { name = "nvim_lsp:lua_ls" },
+      { name = "path" },
+    }))
+
+    opts.mapping = vim.tbl_extend("force", opts.mapping or {}, {
+      ["<Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item()
+          -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+          -- they way you will only jump inside the snippet region
+        elseif luasnip.expand_or_jumpable() then
+          luasnip.expand_or_jump()
+        elseif has_words_before() then
+          cmp.complete()
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
+      ["<S-Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item()
+        elseif luasnip.jumpable(-1) then
+          luasnip.jump(-1)
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
+    })
+  end
+}
