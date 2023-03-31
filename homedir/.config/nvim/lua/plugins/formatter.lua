841bd5b6ab7f6a614cc92de2be27f73e4b4709c3
Jonas Collberg <jonas.collberg@gmail.com>
2023-03-31 14:55:22 +0200

    nvim: Add `formatter.nvim`

---
+++ homedir/.config/nvim/lua/plugins/formatter.lua
@@ -0,0 +1,56 @@
+return {
+  "mhartington/formatter.nvim",
+
+  config = function()
+    local util = require("formatter.util")
+
+    require("formatter").setup({
+      filetype = {
+        haskell = {
+          function()
+            return {
+              exe = "fourmolu",
+              args = {
+                "--stdin-input-file",
+                util.escape_path(util.get_current_buffer_file_path()),
+                "--",
+                "-",
+              },
+              stdin = true,
+            }
+          end,
+        },
+
+        lua = {
+          function()
+            return {
+              exe = "stylua",
+              args = {
+                "--search-parent-directories",
+                "--stdin-filepath",
+                util.escape_path(util.get_current_buffer_file_path()),
+                "--",
+                "-",
+              },
+              stdin = true,
+            }
+          end,
+        },
+
+        ["*"] = {
+          require("formatter.filetypes.any").remove_trailing_whitespace,
+        },
+      },
+    })
+
+    vim.keymap.set("n", "<leader>F", ":Format<cr>", { desc = "Format" })
+
+    -- Autoformat on save.
+    vim.cmd([[
+      augroup FormatAutogroup
+      autocmd!
+      autocmd BufWritePost * FormatWrite
+      augroup END
+    ]])
+  end,
+}
