98d2a79ef616b965badfe224dfc3be359e2e29bc
Jonas Collberg <jonas.collberg@gmail.com>
2023-04-04 12:33:24 +0200

    nvim: Move all key bindings to `config` function

--- homedir/.config/nvim/lua/plugins/telescope.lua
+++ homedir/.config/nvim/lua/plugins/telescope.lua
@@ -6,28 +6,6 @@ return {
       "nvim-lua/plenary.nvim",
     },
 
-    keys = {
-      {
-        "<leader>fi",
-        function()
-          require("telescope.builtin").find_files({
-            cwd = "~/.config/nvim",
-            follow = true,
-          })
-        end,
-        desc = "Find init file",
-      },
-      {
-        "<leader>fp",
-        function()
-          require("telescope.builtin").find_files({
-            cwd = require("lazy.core.config").options.root,
-          })
-        end,
-        desc = "Find plugin file",
-      },
-    },
-
     config = function()
       require("telescope").setup({
         extensions = {
@@ -45,6 +23,19 @@ return {
       vim.lsp.handlers["textDocument/documentSymbol"] =
         builtin.lsp_document_symbols
       vim.lsp.handlers["workspace/symbol"] = builtin.lsp_workspace_symbols
+
+      vim.keymap.set("n", "<leader>fi", function()
+        require("telescope.builtin").find_files({
+          cwd = "~/.config/nvim",
+          follow = true,
+        })
+      end, { desc = "Find init file" })
+
+      vim.keymap.set("n", "<leader>fp", function()
+        require("telescope.builtin").find_files({
+          cwd = require("lazy.core.config").options.root,
+        })
+      end, { desc = "Find plugin file" })
 
       vim.keymap.set(
         "n",
