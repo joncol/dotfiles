9507f72e5e6f8efc0d6ab59a480a1edf64305df8
Jonas Collberg <jonas.collberg@gmail.com>
2023-04-11 13:11:13 +0200

    nvim: Simplify bindings for `harpoon`

--- homedir/.config/nvim/lua/plugins/harpoon.lua
+++ homedir/.config/nvim/lua/plugins/harpoon.lua
@@ -7,36 +7,16 @@ return {
     local mark = require("harpoon.mark")
     local ui = require("harpoon.ui")
 
-    vim.keymap.set("n", "<localleader>m", function()
-      mark.add_file()
-    end)
-
-    vim.keymap.set("n", "<localleader>h", function()
+    vim.keymap.set("n", "gH", function()
       ui.toggle_quick_menu()
     end)
 
-    vim.keymap.set("n", "<localleader>1", function()
-      ui.nav_file(1)
-    end)
-
-    vim.keymap.set("n", "<localleader>2", function()
-      ui.nav_file(2)
-    end)
-
-    vim.keymap.set("n", "<localleader>3", function()
-      ui.nav_file(3)
-    end)
-
-    vim.keymap.set("n", "<localleader>4", function()
-      ui.nav_file(4)
-    end)
-
-    vim.keymap.set("n", "<localleader><tab>", function()
-      ui.nav_next()
-    end)
-
-    vim.keymap.set("n", "<localleader><S-tab>", function()
-      ui.nav_prev()
-    end)
+    vim.keymap.set("n", "gh", function()
+      if vim.v.count > 0 then
+        ui.nav_file(vim.v.count)
+      else
+        mark.toggle_file()
+      end
+    end, { silent = true })
   end,
 }
