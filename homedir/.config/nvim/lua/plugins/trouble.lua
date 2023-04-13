c1a48f34d0a88e2bb1e2c9b2df3fea553befede6
Jonas Collberg <jonas.collberg@gmail.com>
2023-04-13 09:12:35 +0200

    nvim: Add `Trouble` bindings

--- homedir/.config/nvim/lua/plugins/trouble.lua
+++ homedir/.config/nvim/lua/plugins/trouble.lua
@@ -1,11 +1,44 @@
 return {
   "folke/trouble.nvim",
   config = function()
-    require("trouble").setup({})
     vim.keymap.set(
       "n",
-      "<localleader>t",
+      "<localleader>tt",
       "<cmd>TroubleToggle<cr>",
+      { silent = true, noremap = true }
+    )
+
+    vim.keymap.set("n", "<localleader>tn", function()
+      require("trouble").next({ skip_groups = true, jump = true })
+    end, { silent = true, noremap = true })
+
+    vim.keymap.set("n", "<localleader>tp", function()
+      require("trouble").previous({ skip_groups = true, jump = true })
+    end, { silent = true, noremap = true })
+
+    vim.keymap.set(
+      "n",
+      "<localleader>tq",
+      "<cmd>TroubleToggle quickfix<cr>",
+      { silent = true, noremap = true }
+    )
+    vim.keymap.set(
+      "n",
+      "<localleader>tl",
+      "<cmd>TroubleToggle loclist<cr>",
+      { silent = true, noremap = true }
+    )
+
+    vim.keymap.set(
+      "n",
+      "<localleader>tw",
+      "<cmd>TroubleToggle workspace_diagnostics<cr>",
+      { silent = true, noremap = true }
+    )
+    vim.keymap.set(
+      "n",
+      "<localleader>td",
+      "<cmd>TroubleToggle document_diagnostics<cr>",
       { silent = true, noremap = true }
     )
   end,
