8c77f91c3bc170a155e4a97d9a29e42110ff8915
Jonas Collberg <jonas.collberg@gmail.com>
2023-04-14 12:12:35 +0200

    nvim: Reformat file

--- homedir/.config/nvim/lua/plugins/indent_blankline.lua
+++ homedir/.config/nvim/lua/plugins/indent_blankline.lua
@@ -4,10 +4,10 @@ return {
   config = function()
     vim.opt.list = true
     vim.opt.listchars = ""
-    -- vim.opt.listchars:append "space:⋅"
+    -- vim.opt.listchars:append("space:⋅")
     vim.opt.listchars:append("trail:⋅")
     vim.opt.listchars:append("tab:»·")
-    -- vim.opt.listchars:append "eol:↴"
+    -- vim.opt.listchars:append("eol:↴")
 
     require("indent_blankline").setup({
       space_char_blankline = " ",
