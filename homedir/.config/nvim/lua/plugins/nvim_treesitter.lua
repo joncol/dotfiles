22a5cadb794ce0aadcf93b4f47c19eff8cf142b9
Jonas Collberg <jonas.collberg@gmail.com>
2023-04-18 10:23:46 +0200

    nvim: Add `nvim-treesitter-context`

--- homedir/.config/nvim/lua/plugins/nvim_treesitter.lua
+++ homedir/.config/nvim/lua/plugins/nvim_treesitter.lua
@@ -1,35 +1,46 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  config = function()
-    require("nvim-treesitter").setup({
-      ensure_installed = {
-        "bash",
-        "haskell",
-        "html",
-        "javascript",
-        "json",
-        "lua",
-        "markdown",
-        "markdown_inline",
-        "python",
-        "query",
-        "regex",
-        "tsx",
-        "typescript",
-        "vim",
-        "yaml",
-      },
-    })
-    vim.cmd("TSUpdate")
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = { "c", "lua", "vim", "haskell", "query" },
-      sync_install = false,
-      auto_install = true,
-      ignore_install = {},
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-      },
-    })
-  end,
+  {
+    "nvim-treesitter/nvim-treesitter",
+
+    config = function()
+      require("nvim-treesitter").setup({
+        ensure_installed = {
+          "bash",
+          "haskell",
+          "html",
+          "javascript",
+          "json",
+          "lua",
+          "markdown",
+          "markdown_inline",
+          "python",
+          "query",
+          "regex",
+          "tsx",
+          "typescript",
+          "vim",
+          "yaml",
+        },
+      })
+      vim.cmd("TSUpdate")
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = { "c", "lua", "vim", "haskell", "query" },
+        sync_install = false,
+        auto_install = true,
+        ignore_install = {},
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = false,
+        },
+      })
+    end,
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+
+    config = function()
+      require("treesitter-context").setup()
+    end,
+  },
 }
