9980b0b7ab86243a4f8904af946a225f013c1cd3
Jonas Collberg <jonas.collberg@gmail.com>
2023-04-20 09:18:00 +0200

    nvim: Update Treesitter settings

--- homedir/.config/nvim/lua/plugins/nvim_treesitter.lua
+++ homedir/.config/nvim/lua/plugins/nvim_treesitter.lua
@@ -3,28 +3,18 @@ return {
     "nvim-treesitter/nvim-treesitter",
 
     config = function()
-      require("nvim-treesitter").setup({
-        ensure_installed = {
-          "bash",
-          "haskell",
-          "html",
-          "javascript",
-          "json",
-          "lua",
-          "markdown",
-          "markdown_inline",
-          "python",
-          "query",
-          "regex",
-          "tsx",
-          "typescript",
-          "vim",
-          "yaml",
-        },
-      })
+      require("nvim-treesitter").setup()
       vim.cmd("TSUpdate")
       require("nvim-treesitter.configs").setup({
-        ensure_installed = { "c", "lua", "vim", "haskell", "query" },
+        ensure_installed = {
+          "c",
+          "lua",
+          "vim",
+          "haskell",
+          "query",
+          "markdown",
+          "markdown_inline",
+        },
         sync_install = false,
         auto_install = true,
         ignore_install = {},
