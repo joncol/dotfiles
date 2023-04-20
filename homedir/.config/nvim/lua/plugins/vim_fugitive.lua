fba1e1b39909fa3ebc222958e9771f45118e77cd
Jonas Collberg <jonas.collberg@gmail.com>
2023-04-20 12:07:40 +0200

    nvim: Add binding for `:Git blame`

--- homedir/.config/nvim/lua/plugins/vim_fugitive.lua
+++ homedir/.config/nvim/lua/plugins/vim_fugitive.lua
@@ -1,6 +1,7 @@
 return {
   "tpope/vim-fugitive",
   config = function()
-    vim.keymap.set("n", "<localleader>g", ":Git<cr>", { noremap = true })
+    vim.keymap.set("n", "<localleader>g", ":Git<cr>")
+    vim.keymap.set("n", "<localleader>gb", ":Git blame<cr>")
   end,
 }
