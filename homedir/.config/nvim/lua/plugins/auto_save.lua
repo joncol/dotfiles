4d56dc24f9df6b0e84d0ef7a2a3f4f5a08c94695
Jonas Collberg <jonas.collberg@gmail.com>
2023-04-20 12:05:03 +0200

    nvim: Disable auto-save plugin

--- homedir/.config/nvim/lua/plugins/auto_save.lua
+++ homedir/.config/nvim/lua/plugins/auto_save.lua
@@ -1,8 +1,9 @@
 return {
   "okuuva/auto-save.nvim",
+  enabled = false,
   config = function()
     require("auto-save").setup({
-      trigger_events = { "BufLeave", "FocusLost" },
+      trigger_events = { "BufLeave" },
     })
 
     vim.api.nvim_set_keymap("n", "<localleader>as", ":ASToggle<cr>", {})
