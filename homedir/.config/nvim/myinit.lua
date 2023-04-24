a7c404c9413522800599ce2c809b8b96c0d86ea0
Jonas Collberg <jonas.collberg@gmail.com>
2023-04-24 16:41:34 +0200

    nvim: Avoid scrolling when changing buffers.

--- homedir/.config/nvim/myinit.lua
+++ homedir/.config/nvim/myinit.lua
@@ -77,6 +77,37 @@ vim.cmd([[
   cnoremap <A-f> <S-Right>
 ]])
 
+-- Avoid scrolling when changing buffers.
+-- See: https://vim.fandom.com/wiki/Avoid_scrolling_when_switch_buffers
+vim.cmd([[
+  " Save current view settings on a per-window, per-buffer basis.
+  function! AutoSaveWinView()
+    if !exists("w:SavedBufView")
+      let w:SavedBufView = {}
+    endif
+    let w:SavedBufView[bufnr("%")] = winsaveview()
+  endfunction
+
+  " Restore current view settings.
+  function! AutoRestoreWinView()
+    let buf = bufnr("%")
+    if exists("w:SavedBufView") && has_key(w:SavedBufView, buf)
+      let v = winsaveview()
+      let atStartOfFile = v.lnum == 1 && v.col == 0
+      if atStartOfFile && !&diff
+        call winrestview(w:SavedBufView[buf])
+      endif
+      unlet w:SavedBufView[buf]
+    endif
+  endfunction
+
+  " When switching buffers, preserve window view.
+  if v:version >= 700
+    autocmd BufLeave * call AutoSaveWinView()
+    autocmd BufEnter * call AutoRestoreWinView()
+  endif
+]])
+
 vim.cmd.colorscheme("catppuccin-mocha")
 -- vim.cmd.colorscheme "carbonfox"
 -- vim.cmd.colorscheme "dawnfox"
