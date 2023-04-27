58f10d6394edcf4fcfe60de97062ed759165b430
Jonas Collberg <jonas.collberg@gmail.com>
2023-04-27 14:08:28 +0200

    nvim: Set `blend` option for FTerm

--- homedir/.config/nvim/lua/plugins/fterm.lua
+++ homedir/.config/nvim/lua/plugins/fterm.lua
@@ -2,6 +2,7 @@ return {
   "numToStr/FTerm.nvim",
 
   config = function()
+    require("FTerm").setup({ blend = 30 })
     vim.keymap.set("n", "<A-cr>", ':lua require("FTerm").toggle()<cr>')
     vim.keymap.set(
       "t",
