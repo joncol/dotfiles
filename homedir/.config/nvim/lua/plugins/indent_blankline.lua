b2d652b491ecd088fa903e350b34038636ff6586
Jonas Collberg <jonas.collberg@gmail.com>
2023-04-28 11:19:11 +0200

    nvim: Do not showing trailing whitespace
    Instead rely on autoformatting to clean those up.

--- homedir/.config/nvim/lua/plugins/indent_blankline.lua
+++ homedir/.config/nvim/lua/plugins/indent_blankline.lua
@@ -5,7 +5,7 @@ return {
     vim.opt.list = true
     vim.opt.listchars = ""
     -- vim.opt.listchars:append("space:⋅")
-    vim.opt.listchars:append("trail:⋅")
+    -- vim.opt.listchars:append("trail:⋅")
     vim.opt.listchars:append("tab:»·")
     -- vim.opt.listchars:append("eol:↴")
 
