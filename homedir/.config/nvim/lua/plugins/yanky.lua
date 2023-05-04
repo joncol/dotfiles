c94339ab2bdc6e396ff1d2a2804a208a926eea68
Jonas Collberg <jonas.collberg@gmail.com>
2023-05-04 10:45:39 +0200

    nvim: Fix Yanky bindings
    Make `C-n` and `C-p` fallback to default behavior, when possible.

--- homedir/.config/nvim/lua/plugins/yanky.lua
+++ homedir/.config/nvim/lua/plugins/yanky.lua
@@ -5,8 +5,26 @@ return {
     vim.keymap.set({ "n", "x" }, "P", "<Plug>(YankyPutBefore)")
     vim.keymap.set({ "n", "x" }, "gp", "<Plug>(YankyGPutAfter)")
     vim.keymap.set({ "n", "x" }, "gP", "<Plug>(YankyGPutBefore)")
-    vim.keymap.set("n", "<c-n>", "<Plug>(YankyCycleBackward)")
-    vim.keymap.set("n", "<c-p>", "<Plug>(YankyCycleForward)")
+
+    local keymap = vim.keymap
+    local yanky = require("yanky")
+    keymap.amend = require("keymap-amend")
+
+    keymap.amend("n", "<C-p>", function(f)
+      if yanky.can_cycle() then
+        yanky.cycle(1)
+      else
+        f()
+      end
+    end)
+
+    keymap.amend("n", "<C-n>", function(f)
+      if yanky.can_cycle() then
+        yanky.cycle(-1)
+      else
+        f()
+      end
+    end)
     require("yanky").setup()
   end,
 }
