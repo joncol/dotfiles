51eb2e5b31e8d5898dbacf6d13cd54e64caca6c1
Jonas Collberg <jonas.collberg@gmail.com>
2023-05-04 14:29:01 +0200

    nvim: Change binding for vim-visual-multi, again

--- homedir/.config/nvim/lua/plugins/vim_visual_multi.lua
+++ homedir/.config/nvim/lua/plugins/vim_visual_multi.lua
@@ -1,13 +1,14 @@
 return {
   "mg979/vim-visual-multi",
+
   init = function()
     vim.cmd([[
       let g:VM_show_warnings = 0
       let g:VM_theme = "iceblue"
 
       let g:VM_maps = {}
-      let g:VM_maps["Find Under"] = "<C-S-n>"
-      let g:VM_maps["Find Subword Under"] = "<C-S-n>"
+      let g:VM_maps["Find Under"] = "<A-n>"
+      let g:VM_maps["Find Subword Under"] = "<A-n>"
       let g:VM_maps["Undo"] = "u"
       let g:VM_maps["Redo"] = "<C-r>"
     ]])
