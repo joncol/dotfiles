50edac8608eb27793f503b3083d616c3148014dd
Jonas Collberg <jonas.collberg@gmail.com>
2023-05-05 13:08:01 +0200

    nvim: Switch to bugfix branch of symbols-outline
    This branch supports the preview action.

--- homedir/.config/nvim/lua/plugins/symbols_outline.lua
+++ homedir/.config/nvim/lua/plugins/symbols_outline.lua
@@ -1,7 +1,12 @@
 return {
-  "simrat39/symbols-outline.nvim",
-  config = function ()
+  -- "simrat39/symbols-outline.nvim",
+
+  -- See: https://github.com/simrat39/symbols-outline.nvim/issues/176
+  "enddeadroyal/symbols-outline.nvim",
+  branch = "bugfix/symbol-hover-misplacement",
+
+  config = function()
     require("symbols-outline").setup()
     vim.keymap.set("n", "<localleader>s", "<Cmd>SymbolsOutline<CR>")
-  end
+  end,
 }
