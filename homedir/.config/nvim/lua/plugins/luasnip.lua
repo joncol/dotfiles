38f87d04d69c1621fc0bce53a999ec3e22e50516
Jonas Collberg <jonas.collberg@gmail.com>
2023-05-11 12:43:41 +0200

    nvim: Remove `ternary` snippet

--- homedir/.config/nvim/lua/plugins/luasnip.lua
+++ homedir/.config/nvim/lua/plugins/luasnip.lua
@@ -29,13 +29,6 @@ return {
     -- local ms = ls.multi_snippet
     --
     ls.add_snippets("all", {
-      s("ternary", {
-        -- equivalent to "${1:cond} ? ${2:then} : ${3:else}"
-        i(1, "cond"),
-        t(" ? "),
-        i(2, "then"),
-        t(" : "),
-        i(3, "else"),
       }),
     })
 
