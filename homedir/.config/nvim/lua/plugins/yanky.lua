87dc764024a836e6f558691e40ed377ced582b8f
Jonas Collberg <jonas.collberg@gmail.com>
2023-05-12 16:38:08 +0200

    nvim: Add `yank_history` Telescope extension

--- homedir/.config/nvim/lua/plugins/yanky.lua
+++ homedir/.config/nvim/lua/plugins/yanky.lua
@@ -1,6 +1,36 @@
 return {
   "gbprod/yanky.nvim",
+
+  dependencies = { "nvim-telescope/telescope.nvim" },
+
   config = function()
+    local actions = require("telescope.actions")
+    local utils = require("yanky.utils")
+    local mapping = require("yanky.telescope.mapping")
+
+    require("yanky").setup({
+      picker = {
+        telescope = {
+          default = mapping.put("p"),
+          mappings = {
+            i = {
+              ["<C-n>"] = actions.move_selection_next,
+              ["<C-p>"] = actions.move_selection_previous,
+              ["<C-x>"] = mapping.delete(),
+              ["<C-r>"] = mapping.set_register(utils.get_default_register()),
+              ["<A-CR>"] = mapping.put("P"),
+            },
+            n = {
+              p = mapping.put("p"),
+              P = mapping.put("P"),
+              d = mapping.delete(),
+              r = mapping.set_register(utils.get_default_register()),
+            },
+          },
+        },
+      },
+    })
+
     vim.keymap.set({ "n", "x" }, "p", "<Plug>(YankyPutAfter)")
     vim.keymap.set({ "n", "x" }, "P", "<Plug>(YankyPutBefore)")
     vim.keymap.set({ "n", "x" }, "gp", "<Plug>(YankyGPutAfter)")
@@ -25,6 +55,9 @@ return {
         f()
       end
     end)
-    require("yanky").setup()
+
+    require("telescope").load_extension("yank_history")
+
+    vim.keymap.set("n", "<A-y>", "<Cmd>Telescope yank_history<CR>")
   end,
 }
