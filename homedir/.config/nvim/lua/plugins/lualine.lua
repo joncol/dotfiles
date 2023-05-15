0e6f0e3ea9335ee8bd6b48521e60bb8255dd0a44
Jonas Collberg <jonas.collberg@gmail.com>
2023-05-15 13:10:17 +0200

    nvim: Remove `github_notifications`
    It took 20 ms to start up and I wasn't using it.

--- homedir/.config/nvim/lua/plugins/lualine.lua
+++ homedir/.config/nvim/lua/plugins/lualine.lua
@@ -8,16 +8,5 @@ return {
       section_separators = { left = "", right = "" },
       component_separators = "|",
     },
-    sections = {
-      lualine_a = { "mode" },
-      lualine_b = {
-        "branch",
-        require("github-notifications").statusline_notification_count,
-      },
-      lualine_c = { "filename" },
-      lualine_x = { "encoding", "fileformat", "filetype" },
-      lualine_y = { "progress" },
-      lualine_z = { "location" },
-    },
   },
 }
