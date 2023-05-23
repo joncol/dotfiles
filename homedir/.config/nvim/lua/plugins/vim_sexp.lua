6d48da1f149072c5871838bed975b97030415eca
Jonas Collberg <jonas.collberg@gmail.com>
2023-05-23 11:09:26 +0200

    nvim: Enable vim-sexp for Fennel

--- homedir/.config/nvim/lua/plugins/vim_sexp.lua
+++ homedir/.config/nvim/lua/plugins/vim_sexp.lua
@@ -1,4 +1,9 @@
 return {
   { "tpope/vim-sexp-mappings-for-regular-people" },
-  { "guns/vim-sexp" },
+  {
+    "guns/vim-sexp",
+    config = function()
+      vim.g.sexp_filetypes = vim.g.sexp_filetypes .. ",fennel"
+    end,
+  },
 }
