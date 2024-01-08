512fffb659db53b7d67e310fb56980e091dc9aec
Jonas Collberg <jonas.collberg@gmail.com>
2024-01-08 08:50:20 +0100

    emacs: Switch theme

--- homedir/.emacs.d/init.el
+++ homedir/.emacs.d/init.el
@@ -2,7 +2,7 @@
 (when (file-exists-p custom-file)
   (load custom-file))
 
-(let ((my-theme '"ef-winter"))
+(let ((my-theme '"doom-henna"))
 (defvar jco/theme)
 (setq jco/theme (intern my-theme))
 )
