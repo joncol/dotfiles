e224045052d6820ebb70ca5ac27964e7e58c6e1e
Jonas Collberg <jonas.collberg@gmail.com>
2024-01-08 07:42:13 +0100

    Update work files

---
+++ work/scrive/k3/frontend-elm/.dir-locals.el
@@ -0,0 +1,16 @@
+((css-mode . ((eval . (apheleia-mode -1))))
+
+ (web-mode . ((eval . (apheleia-mode -1))
+              (fill-column . 90)))
+
+ (less-css-mode . ((eval . (apheleia-mode -1))))
+
+ (js-mode . ((eval . (apheleia-mode -1))
+             (fill-column . 90)
+             (evil-shift-width . 2)
+             (js-indent-level . 2)))
+
+ (json-mode . ((eval . (apheleia-mode -1))
+               (eval . (display-line-numbers-mode -1))))
+
+ (scss-mode . ((eval . (apheleia-mode -1)))))
---
+++ work/scrive/k3/frontend/.dir-locals.el
@@ -0,0 +1,16 @@
+((css-mode . ((eval . (apheleia-mode -1))))
+
+ (web-mode . ((eval . (apheleia-mode -1))
+              (fill-column . 120)))
+
+ (less-css-mode . ((eval . (apheleia-mode -1))))
+
+ (js-mode . ((eval . (apheleia-mode -1))
+             (fill-column . 120)
+             (evil-shift-width . 2)
+             (js-indent-level . 2)))
+
+ (json-mode . ((eval . (apheleia-mode -1))
+               (eval . (display-line-numbers-mode -1))))
+
+ (scss-mode . ((eval . (apheleia-mode -1)))))
