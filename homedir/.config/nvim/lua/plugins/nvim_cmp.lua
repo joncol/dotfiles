02914c0dedd0a92ba3e20ad0344ceab1a14f82a0
Jonas Collberg <jonas.collberg@gmail.com>
2024-02-28 07:51:14 +0100

    nvim: Add Copilot configuration

--- homedir/.config/nvim/lua/plugins/nvim_cmp.lua
+++ homedir/.config/nvim/lua/plugins/nvim_cmp.lua
@@ -1,105 +1,115 @@
 return {
-  "hrsh7th/nvim-cmp",
+  {
+    "hrsh7th/nvim-cmp",
 
-  dependencies = {
-    "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-cmdline",
-    "hrsh7th/cmp-git",
-    "saadparwaiz1/cmp_luasnip",
-    "PaterJason/cmp-conjure",
-  },
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-cmdline",
+      "hrsh7th/cmp-git",
+      "saadparwaiz1/cmp_luasnip",
+      "PaterJason/cmp-conjure",
+      "zbirenbaum/copilot-cmp",
+    },
 
-  config = function()
-    local cmp = require("cmp")
-    local luasnip = require("luasnip")
+    config = function()
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
 
-    local has_words_before = function()
-      local cursor = vim.api.nvim_win_get_cursor(0)
-      return (
-        vim.api.nvim_buf_get_lines(0, cursor[1] - 1, cursor[1], true)[1] or ""
-      ):sub(cursor[2], cursor[2]):match("%s")
-    end
+      local has_words_before = function()
+        local cursor = vim.api.nvim_win_get_cursor(0)
+        return (
+          vim.api.nvim_buf_get_lines(0, cursor[1] - 1, cursor[1], true)[1] or ""
+        ):sub(cursor[2], cursor[2]):match("%s")
+      end
 
-    cmp.setup({
-      snippet = {
-        expand = function(args)
-          luasnip.lsp_expand(args.body)
-        end,
-      },
-
-      mapping = cmp.mapping.preset.insert({
-        ["<Tab>"] = cmp.mapping(function(fallback)
-          -- You could replace the expand_or_jumpable() calls with
-          -- expand_or_locally_jumpable() they way you will only jump inside
-          -- the snippet region
-          if luasnip.expand_or_jumpable() then
-            luasnip.expand_or_jump()
-          elseif cmp.visible() then
-            cmp.select_next_item()
-          elseif has_words_before() then
-            cmp.complete()
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif luasnip.jumpable(-1) then
-            luasnip.jump(-1)
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-
-        ["<CR>"] = cmp.mapping.confirm({ select = true }),
-      }),
-
-      sources = cmp.config.sources({
-        { name = "nvim_lsp" },
-        { name = "luasnip" },
-        { name = "conjure" },
-        { name = "buffer" },
-      }, { name = "buffer" }),
-    })
-
-    cmp.setup.filetype("gitcommit", {
-      sources = cmp.config.sources({
-        { name = "cmp_git" },
-      }, {
-        { name = "buffer" },
-      }),
-    })
-
-    cmp.setup.cmdline("/", {
-      mapping = cmp.mapping.preset.cmdline(),
-      sources = {
-        { name = "buffer" },
-      },
-    })
-
-    cmp.setup.cmdline(":", {
-      mapping = cmp.mapping.preset.cmdline({
-        ["<CR>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.mapping.confirm({ select = false })
-          else
-            fallback()
-          end
-        end),
-      }),
-      sources = cmp.config.sources({
-        { name = "path" },
-      }, {
-        {
-          name = "cmdline",
-          option = {
-            ignore_cmds = { "Man", "!" },
-          },
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
         },
-      }),
-    })
-  end,
+
+        mapping = cmp.mapping.preset.insert({
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            -- You could replace the expand_or_jumpable() calls with
+            -- expand_or_locally_jumpable() they way you will only jump inside
+            -- the snippet region
+            if luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            elseif cmp.visible() then
+              cmp.select_next_item()
+            elseif has_words_before() then
+              cmp.complete()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        }),
+
+        sources = cmp.config.sources({
+          { name = "copilot" },
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "conjure" },
+          { name = "buffer" },
+        }, { name = "buffer" }),
+      })
+
+      cmp.setup.filetype("gitcommit", {
+        sources = cmp.config.sources({
+          { name = "cmp_git" },
+        }, {
+          { name = "buffer" },
+        }),
+      })
+
+      cmp.setup.cmdline("/", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = "buffer" },
+        },
+      })
+
+      cmp.setup.cmdline(":", {
+        mapping = cmp.mapping.preset.cmdline({
+          ["<CR>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.mapping.confirm({ select = false })
+            else
+              fallback()
+            end
+          end),
+        }),
+        sources = cmp.config.sources({
+          { name = "path" },
+        }, {
+          {
+            name = "cmdline",
+            option = {
+              ignore_cmds = { "Man", "!" },
+            },
+          },
+        }),
+      })
+    end,
+  },
+  {
+    "zbirenbaum/copilot-cmp",
+    config = function()
+      require("copilot_cmp").setup()
+    end,
+  },
 }
