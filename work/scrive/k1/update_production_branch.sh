c84c8205e662f770e8fec99fc5d491579649a837
Jonas Collberg <jonas.collberg@gmail.com>
2024-07-04 11:18:03 +0200

    scrive: Update `update_production_branch.sh` script

--- work/scrive/k1/update_production_branch.sh
+++ work/scrive/k1/update_production_branch.sh
@@ -7,8 +7,10 @@ set -o xtrace # Trace all commands being executed.
 
 date=$(date +%Y-%m-%d)
 git checkout production
-git pull --ff-only
+git pull --ff-only origin
 git tag "production-${date}" # Create a tag.
 git push -u origin production-${date}:production-${date} # Push the tag.
 git reset --hard staging # Production branch is already merged into staging, so we can reset our local production branch to that.
-git push --force-with-lease # Push merge of staging and old production into production branch.
+git tag release/${RELEASE_NUMBER}
+git push --force-with-lease origin # Push merge of staging and old production into production branch.
+git push origin release/${RELEASE_NUMBER} # Push the tag
