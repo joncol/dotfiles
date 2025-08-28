72a45aef733d1d0d7963a59dc3801d043645393e
Jonas Collberg <jonas.collberg@gmail.com>
2025-08-28 15:40:25 +0200

    scrive/kontrakcja: Remove deployment scripts

--- work/scrive/kontrakcja/promote_master_to_staging.sh
+++
@@ -1,8 +0,0 @@
-git checkout staging && git pull --ff-only
-git checkout master && git pull --ff-only
-git merge staging
-# fix conflicts if needed
-git push
-git checkout staging
-git merge master
-git push
